5d9f36fa870cf702304c6d5a7d0192aaab5ad8c8
Mateus Azevedo <mateus6@gmail.com>
2015-05-05 15:53:31 -0300

    Alterando tamanho da descricao do produto para evitar numero em cima do endereco

--- library/Wms/Module/Expedicao/Printer/EtiquetaSeparacao.php
+++ library/Wms/Module/Expedicao/Printer/EtiquetaSeparacao.php
@@ -290,6 +290,13 @@ class EtiquetaSeparacao extends Pdf
         $impressao = "CODIGO:$etiqueta[codProduto] - GRADE:$etiqueta[grade]\n";
         $this->MultiCell(100, 3.9, $impressao, 0, 'L');
         $this->SetFont('Arial', 'B', 13);
+
+        $tamanhoSringProduto = strlen($etiqueta['produto']);
+        if ($tamanhoSringProduto >= 35) {
+            $this->SetFont('Arial', 'B', 11);
+        } else {
+            $this->SetFont('Arial', 'B', 13);
+        }
         $impressao = substr(trim($etiqueta['produto']),0,70)."\n";
         $this->MultiCell(100, 3.9, $impressao, 0, 'L');
         $this->SetFont('Arial', 'B', 9);
