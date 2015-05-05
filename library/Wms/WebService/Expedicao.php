fef3a0977b88266d2ae0975282ea3c061dcdca4f
Lucas Chinelate <lucaschinelate@hotmail.com>
2015-05-05 14:30:08 -0300

    Teste

--- library/Wms/WebService/Expedicao.php
+++ library/Wms/WebService/Expedicao.php
@@ -352,7 +352,7 @@ class Wms_WebService_Expedicao extends Wms_WebService
         foreach ($pedidosEn as $pedidoEn) {
             $itinerario = new itinerario();
             if ($pedidoEn->getItinerario() == null) {
-                throw new \Exception($pedidoEn->getItinerario());
+                throw new \Exception($pedidoEn->getItinerario()->getId());
                 $itinerario->idItinerario = $pedidoEn->getItinerario()->getId();
                 $itinerario->nomeItinerario = $pedidoEn->getItinerario()->getDescricao();
             } else {
