97afc05b2300182077e82fa4ec57162eeda80f02
Lucas Chinelate <lucaschinelate@hotmail.com>
2015-05-26 17:03:55 -0300

    Exibindo o código de barras do produto no relatório de ondas

--- application/modules/web/controllers/Relatorio/RelatorioOndasController.php
+++ application/modules/web/controllers/Relatorio/RelatorioOndasController.php
@@ -18,7 +18,7 @@ class Web_Relatorio_RelatorioOndasController extends Action
             $status         = $values['status'];
             /** @var \Wms\Domain\Entity\Ressuprimento\OndaRessuprimentoRepository $ondaRessuprimentoRepo */
             $ondaRessuprimentoRepo = $this->em->getRepository("wms:Ressuprimento\OndaRessuprimento");
-            $result = $ondaRessuprimentoRepo->getOndasEmAbertoCompleto($dataInicial,$dataFinal,$status);
+            $result = $ondaRessuprimentoRepo->getOndasEmAbertoCompleto($dataInicial,$dataFinal,$status,false, null,null,null,true);
             $this->exportPDF($result,'relatorio-Onda','Ondas de Ressuprimento Abertas - '.count($result).' ondas','L');
         }
 
