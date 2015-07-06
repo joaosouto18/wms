0f2fcff90a6d7a07c1591a63b24d9186b6f22632
unknown <wms@CABERNET.prem1um.intra>
2015-07-06 15:37:07 -0300

    tirando throw exception

--- library/Wms/Domain/Entity/Expedicao/MapaSeparacaoRepository.php
+++ library/Wms/Domain/Entity/Expedicao/MapaSeparacaoRepository.php
@@ -72,7 +72,6 @@ class MapaSeparacaoRepository extends EntityRepository
             if ($produto['VOLUME'] != "0") $arrayFiltro['produtoVolume'] = $produto['VOLUME'];
             $produtosEn = $mapaSeparacaoProdutoRepo->findBy($arrayFiltro);
             foreach ($produtosEn as $produtoEn) {
-                throw new \Exception('chegou aqui 3');
 
                 $produtoEn->setIndConferido('S');
                 $this->getEntityManager()->persist($produtoEn);
