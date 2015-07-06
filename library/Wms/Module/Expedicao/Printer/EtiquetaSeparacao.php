636f872551cc39937404e22cd4577074d57bf4ba
Rodrigo Dantley <rodrigodantley@gmail.com>
2015-07-06 11:04:06 -0300

    etiquetas

--- library/Wms/Module/Expedicao/Printer/EtiquetaSeparacao.php
+++ library/Wms/Module/Expedicao/Printer/EtiquetaSeparacao.php
@@ -359,6 +359,9 @@ class EtiquetaSeparacao extends Pdf
         $strReimpressao = "";
         if ($reimpressao == true) {$strReimpressao = "Reimpressão";}
 
+        $this->AddPage();
+        $yImage = 33;
+
         $this->total=$countEtiquetas;
         $this->modelo = $modelo;
         $this->strReimpressao = $strReimpressao;
@@ -385,7 +388,7 @@ class EtiquetaSeparacao extends Pdf
         $this->SetFont('Arial', 'B', 9);
         $impressao = substr(utf8_decode("FORNECEDOR:$etiqueta[fornecedor]"),0,40) . "\n";
         $this->MultiCell(100, 3.9, $impressao, 0, 'L');
-        $this->Image(@CodigoBarras::gerarNovo($etiqueta['codBarras']), 29, 36, 68,17);
+        $this->Image(@CodigoBarras::gerarNovo($etiqueta['codBarras']), 29, $yImage, 68,17);
         $this->Image(APPLICATION_PATH . '/../public/img/premium-etiqueta.gif', 4.1, 1.5, 20,5);
         $this->SetFont('Arial', 'B', 13);
         $this->SetY(36);
