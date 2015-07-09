2b5fe3ebcb15dd4b845673289d05b51c2df53ca3
Mateus Azevedo <mateus6@gmail.com>
2015-07-09 11:27:41 -0300

    correcao finalizacao

--- application/modules/expedicao/controllers/ConferenciaController.php
+++ application/modules/expedicao/controllers/ConferenciaController.php
@@ -45,11 +45,11 @@ class Expedicao_ConferenciaController extends Action
                 } else {
                     $result = 'Senha informada não é válida';
                     $this->addFlashMessage('error', $result);
-                    if ($origin == "expedicao") {
-                        $this->_redirect('/expedicao');
-                    } else {
-                        $this->_redirect('/expedicao/os/index/id/' . $idExpedicao);
-                    }
+                }
+                if ($origin == "expedicao") {
+                    $this->_redirect('/expedicao');
+                } else {
+                    $this->_redirect('/expedicao/os/index/id/' . $idExpedicao);
                 }
             } else {
                 $result = $expedicaoRepo->finalizarExpedicao($idExpedicao,$centrais,true, 'M');
