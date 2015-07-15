3db59c200806d507b337ed38d322af6b5ee725f3
Rodrigo Dantley <rodrigodantley@gmail.com>
2015-07-15 16:40:42 -0300

    relatorio de produto recebido com limitacao de caracter

--- library/Wms/Module/Web/Report/Recebimento/ProdutoRecebido.php
+++ library/Wms/Module/Web/Report/Recebimento/ProdutoRecebido.php
@@ -22,9 +22,9 @@ class ProdutoRecebido extends Report
         \Zend_Controller_Front::getInstance()->setParam('noViewRenderer', true);
 
         $pdf = new \Wms\Module\Web\Pdf('L', 'mm', 'A4');
-        $pdf->setTitle('Relatório de Produtos Recebidos')
-                ->setLabelHeight(6)
-                ->setColHeight(7);
+        $pdf->setTitle(utf8_decode('Relatório de Produtos Recebidos'))
+            ->setLabelHeight(6)
+            ->setColHeight(7);
 
         // header
         $pdf->addLabel(0, 25, 'Cod. Receb.', 0, 0, 'L');
@@ -33,24 +33,24 @@ class ProdutoRecebido extends Report
         $pdf->addLabel(0, 15, 'Serie', 0, 0, 'L');
         $pdf->addLabel(0, 30, 'Cod. Produto', 0, 0, 'L');
         $pdf->addLabel(0, 20, 'Grade', 0, 0, 'L');
-        $pdf->addLabel(0, 70, 'Descrição', 0, 0, 'L');
+        $pdf->addLabel(0, 70, utf8_decode('Descrição'), 0, 0, 'L');
         $pdf->addLabel(0, 10, 'Qtd.:', 0, 0, 'L');
         $pdf->addLabel(0, 12, 'Nota', 0, 0, 'C');
         $pdf->addLabel(0, 22, 'Conferida', 0, 0, 'C');
-        $pdf->addLabel(0, 20, 'Divergência', 0, 1, 'C');
+        $pdf->addLabel(0, 20, utf8_decode('Divergência'), 0, 1, 'C');
 
 
         foreach ($produtos as $produto) {
 
             $dataRecebimento = \DateTime::createFromFormat('Y-m-d H:i:s', $produto['DTH_FINAL_RECEB']);
-            
+
             $pdf->addCol(0, 25, $produto['COD_RECEBIMENTO'], 0, 0, 'L');
             $pdf->addCol(0, 30, $dataRecebimento->format('d/m/Y'), 0, 0, 'L');
             $pdf->addCol(0, 25, $produto['NUM_NOTA_FISCAL'], 0, 0, 'L');
             $pdf->addCol(0, 15, $produto['COD_SERIE_NOTA_FISCAL'], 0, 0, 'L');
             $pdf->addCol(0, 30, $produto['COD_PRODUTO'], 0, 0, 'L');
             $pdf->addCol(0, 20, $produto['DSC_GRADE'], 0, 0, 'L');
-            $pdf->addCol(0, 70, $produto['DSC_PRODUTO'], 0, 0, 'L');
+            $pdf->addCol(0, 70, substr($produto['DSC_PRODUTO'],0,40), 0, 0, 'L');
             $pdf->addCol(0, 10, '', 0, 0, 'L');
             $pdf->addCol(0, 12, $produto['QTD_ITEM'], 0, 0, 'C');
             $pdf->addCol(0, 22, $produto['QTD_CONFERIDA'], 0, 0, 'C');
@@ -59,8 +59,8 @@ class ProdutoRecebido extends Report
 
         // page
         $pdf->AddPage()
-                ->render()
-                ->Output('relatorio.pdf', 'D');
+            ->render()
+            ->Output('relatorio.pdf', 'D');
     }
 
 }
