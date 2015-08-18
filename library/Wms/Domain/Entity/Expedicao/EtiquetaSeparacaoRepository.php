3478b1347a7fd93464236ddc0578f59a88ba70d4
Lucas Chinelate <lucaschinelate@hotmail.com>
2015-08-18 18:38:53 -0300

    Correcao SeparacaoPulmao

--- library/Wms/Domain/Entity/Expedicao/EtiquetaSeparacaoRepository.php
+++ library/Wms/Domain/Entity/Expedicao/EtiquetaSeparacaoRepository.php
@@ -273,11 +273,13 @@ class EtiquetaSeparacaoRepository extends EntityRepository
             ->select(' es.codEntrega, es.codBarras, es.codCarga, es.linhaEntrega, es.itinerario, es.cliente, es.codProduto, es.produto,
                     es.grade, es.fornecedor, es.tipoComercializacao, es.linhaSeparacao, es.codEstoque, es.codExpedicao,
                     es.placaExpedicao, es.codClienteExterno, es.tipoCarga, es.codCargaExterno, es.tipoPedido, etq.codEtiquetaMae,
-                    IDENTITY(etq.produtoEmbalagem) as codProdutoEmbalagem, etq.qtdProduto, p.id pedido, etq.depositoEndereco endereco
+                    IDENTITY(etq.produtoEmbalagem) as codProdutoEmbalagem, etq.qtdProduto, p.id pedido, de.descricao endereco, c.sequencia
                 ')
             ->from('wms:Expedicao\VEtiquetaSeparacao','es')
             ->innerJoin('wms:Expedicao\Pedido', 'p' , 'WITH', 'p.id = es.codEntrega')
+            ->innerJoin('wms:Expedicao\Carga', 'c' , 'WITH', 'c.id = es.codCarga')
             ->innerJoin('wms:Expedicao\EtiquetaSeparacao', 'etq' , 'WITH', 'etq.id = es.codBarras')
+            ->leftjoin('etq.codDepositoEndereco', 'de')
             ->where('es.codExpedicao = :idExpedicao')
             ->distinct(true)
             ->setParameter('idExpedicao', $idExpedicao);
