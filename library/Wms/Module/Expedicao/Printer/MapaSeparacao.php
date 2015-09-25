9130b91926184f2219e03ed52b86dfd0c4659104
Rodrigo Dantley <rodrigodantley@gmail.com>
2015-09-25 14:37:01 -0300

    aumento de fonte de etiqueta de mapa de separacao

--- library/Wms/Module/Expedicao/Printer/MapaSeparacao.php
+++ library/Wms/Module/Expedicao/Printer/MapaSeparacao.php
@@ -17,28 +17,28 @@ class MapaSeparacao extends Pdf
     public function Header()
     {
         //Select Arial bold 8
-        $this->SetFont('Arial','B',16);
+        $this->SetFont('Arial','B',10);
         $this->Cell(200, 3, utf8_decode("MAPA DE SEPARAÇÃO " . $this->idMapa), 0, 1,"C");
-        $this->Cell(20, 1, "___________________________________________________________________________________________________________________", 0, 1);
+        $this->Cell(20, 1, "__________________________________________________________________________________________________", 0, 1);
         $this->Cell(20, 3, "", 0, 1);
-        $this->SetFont('Arial','B',16);
-        $this->Cell(35, 6, utf8_decode("EXPEDIÇÃO: "), 0, 0);
-        $this->SetFont('Arial',null,16);
-        $this->Cell(7, 6, utf8_decode( $this->idExpedicao), 0, 1);
-        $this->SetFont('Arial','B',16);
-        $this->Cell(31, 6, utf8_decode("QUEBRAS: "), 0, 0);
-        $this->SetFont('Arial',null,14);
-        $this->Cell(23, 6, utf8_decode($this->quebrasEtiqueta), 0, 1);
+        $this->SetFont('Arial','B',10);
+        $this->Cell(24, 4, utf8_decode("EXPEDIÇÃO: "), 0, 0);
+        $this->SetFont('Arial',null,10);
+        $this->Cell(4, 4, utf8_decode( $this->idExpedicao), 0, 1);
+        $this->SetFont('Arial','B',10);
+        $this->Cell(20, 4, utf8_decode("QUEBRAS: "), 0, 0);
+        $this->SetFont('Arial',null,10);
+        $this->Cell(20, 4, utf8_decode($this->quebrasEtiqueta), 0, 1);
 
 
         $this->Cell(20, 4, "", 0, 1);
-        $this->SetFont('Arial', 'B', 16);
-        $this->Cell(36, 8, utf8_decode("Endereço") ,1, 0);
-        $this->Cell(40, 8, utf8_decode("Cod.Produto") ,1, 0);
-        $this->Cell(28, 8, utf8_decode("Grade") ,1, 0);
-        $this->Cell(103, 8, utf8_decode("Produto") ,1, 0);
-        $this->Cell(39, 8, utf8_decode("Embalagem") ,1, 0);
-        $this->Cell(38, 8, utf8_decode("Quantidade") ,1, 1);
+        $this->SetFont('Arial', 'B', 8);
+        $this->Cell(20, 5, utf8_decode("Endereço") ,1, 0);
+        $this->Cell(20, 5, utf8_decode("Cod.Produto") ,1, 0);
+        $this->Cell(20, 5, utf8_decode("Grade") ,1, 0);
+        $this->Cell(80, 5, utf8_decode("Produto") ,1, 0);
+        $this->Cell(35, 5, utf8_decode("Embalagem") ,1, 0);
+        $this->Cell(20, 5, utf8_decode("Quantidade") ,1, 1);
         $this->Cell(20, 1, "", 0, 1);
     }
 
@@ -48,13 +48,13 @@ class MapaSeparacao extends Pdf
         $this->SetY(-52);
 
         $this->SetFont('Arial',null,10);
-        $this->Cell(20, 1, "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -", 0, 1);
-        $this->SetFont('Arial','B',14);
+        $this->Cell(20, 1, "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -", 0, 1);
+        $this->SetFont('Arial','B',9);
 
         $this->Cell(4, 10, utf8_decode("MAPA DE SEPARAÇÃO " . $this->idMapa), 0, 1);
 
-        $this->Image(@CodigoBarras::gerarNovo($this->idMapa), 190, 180, 75);
-        $this->SetFont('Arial','B',10);
+        $this->Image(@CodigoBarras::gerarNovo($this->idMapa), 150, 250, 50);
+        $this->SetFont('Arial','B',7);
         //Go to 1.5 cm from bottom
         $this->Cell(20, 3, utf8_decode(date('d/m/Y')." às ".date('H:i')), 0, 1, "L");
     }
@@ -85,20 +85,20 @@ class MapaSeparacao extends Pdf
 
             $this->AddPage();
             foreach ($produtos as $produto) {
-                $this->SetFont('Arial',  null, 16);
+                $this->SetFont('Arial',  null, 8);
                 $endereco = $produto->getProdutoEmbalagem()->getEndereco();
                 $dscEndereco = "";
                 if ($endereco != null) {
                     $dscEndereco = $endereco->getDescricao();
                 }
                 $embalagem = $produto->getProdutoEmbalagem();
-                $this->Cell(36, 7, utf8_decode($dscEndereco) ,0, 0);
-                $this->Cell(40, 7, utf8_decode($produto->getCodProduto()) ,0, 0);
-                $this->Cell(28, 7, utf8_decode($produto->getDscGrade()) ,0, 0);
-                $this->Cell(103, 7, substr(utf8_decode($produto->getProduto()->getDescricao()),0,54) ,0, 0);
-                $this->Cell(39, 7, utf8_decode($embalagem->getDescricao() . " (". $embalagem->getQuantidade() . ")") ,0, 0);
-                $this->Cell(38, 7, utf8_decode($produto->getQtdSeparar()) ,0, 1, 'C');
-                $this->Cell(20, 3, "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -", 0, 1);
+                $this->Cell(20, 4, utf8_decode($dscEndereco) ,0, 0);
+                $this->Cell(20, 4, utf8_decode($produto->getCodProduto()) ,0, 0);
+                $this->Cell(20, 4, utf8_decode($produto->getDscGrade()) ,0, 0);
+                $this->Cell(80, 4, substr(utf8_decode($produto->getProduto()->getDescricao()),0,54) ,0, 0);
+                $this->Cell(35, 4, utf8_decode($embalagem->getDescricao() . " (". $embalagem->getQuantidade() . ")") ,0, 0);
+                $this->Cell(20, 4, utf8_decode($produto->getQtdSeparar()) ,0, 1, 'C');
+                $this->Cell(20, 1, "- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -", 0, 1);
                 $this->Cell(20, 1, "", 0, 1);
             }
 
