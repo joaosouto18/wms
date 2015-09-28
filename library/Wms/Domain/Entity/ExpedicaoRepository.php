f9e127e1ca8450bc7b4ccda38e614cae1592e7b4
Lucas Chinelate <lucaschinelate@hotmail.com>
2015-09-28 16:29:29 -0300

    correção ressuprimento

--- library/Wms/Domain/Entity/ExpedicaoRepository.php
+++ library/Wms/Domain/Entity/ExpedicaoRepository.php
@@ -69,7 +69,7 @@ class ExpedicaoRepository extends EntityRepository
     {
         $Query = "SELECT PP.COD_PRODUTO,
                          PP.DSC_GRADE,
-                         SUM (NVL(PP.QUANTIDADE,0)) - SUM(NVL(PP.QTD_CORTADA)) as QTD
+                         SUM (NVL(PP.QUANTIDADE,0)) - SUM(NVL(PP.QTD_CORTADA,0)) as QTD
                     FROM PEDIDO P
                     LEFT JOIN PEDIDO_PRODUTO PP ON PP.COD_PEDIDO = P.COD_PEDIDO
                     LEFT JOIN CARGA          C ON C.COD_CARGA = P.COD_CARGA
@@ -77,7 +77,7 @@ class ExpedicaoRepository extends EntityRepository
                     WHERE P.COD_PEDIDO NOT IN (SELECT COD_PEDIDO FROM ONDA_RESSUPRIMENTO_PEDIDO)
                           AND E.COD_EXPEDICAO IN (".$expedicoes.")
                           AND P.CENTRAL_ENTREGA = $filialExterno
-                          AND (SUM(NVL(PP.QUANTIDADE,0)) - SUM(NVL(PP.QTD_CORTADA)) > 0
+                          AND (SUM(NVL(PP.QUANTIDADE,0)) - SUM(NVL(PP.QTD_CORTADA,0)) > 0
                     GROUP BY PP.COD_PRODUTO, PP.DSC_GRADE";
         $result = $this->getEntityManager()->getConnection()->query($Query)-> fetchAll(\PDO::FETCH_ASSOC);
         return $result;
@@ -87,7 +87,7 @@ class ExpedicaoRepository extends EntityRepository
     {
         $Query = "SELECT PP.COD_PRODUTO,
                          PP.DSC_GRADE,
-                         SUM (NVL(PP.QUANTIDADE,0)) - SUM(NVL(PP.QTD_CORTADA)) as QTD,
+                         SUM (NVL(PP.QUANTIDADE,0)) - SUM(NVL(PP.QTD_CORTADA,0)) as QTD,
                          E.COD_EXPEDICAO
                     FROM PEDIDO P
                     LEFT JOIN PEDIDO_PRODUTO PP ON PP.COD_PEDIDO = P.COD_PEDIDO
@@ -97,7 +97,7 @@ class ExpedicaoRepository extends EntityRepository
                     WHERE P.COD_PEDIDO NOT IN (SELECT COD_PEDIDO FROM ONDA_RESSUPRIMENTO_PEDIDO)
                           AND E.COD_EXPEDICAO IN (".$expedicoes.")
                           AND P.CENTRAL_ENTREGA = $filialExterno
-                          AND SUM (NVL(PP.QUANTIDADE,0)) - SUM(NVL(PP.QTD_CORTADA)) as QTD
+                          AND SUM (NVL(PP.QUANTIDADE,0)) - SUM(NVL(PP.QTD_CORTADA,0)) > 0
                     GROUP BY PP.COD_PRODUTO, PP.DSC_GRADE,E.COD_EXPEDICAO";
         $result = $this->getEntityManager()->getConnection()->query($Query)-> fetchAll(\PDO::FETCH_ASSOC);
         return $result;
@@ -116,6 +116,7 @@ class ExpedicaoRepository extends EntityRepository
                     WHERE P.COD_PEDIDO NOT IN (SELECT COD_PEDIDO FROM ONDA_RESSUPRIMENTO_PEDIDO)
                           AND E.COD_EXPEDICAO IN (".$expedicoes.")
                           AND P.CENTRAL_ENTREGA = $filialExterno
+                          AND (PP.QUANTIDADE - PP.QTD_CORTADA) > 0
                           ";
 
         $result = $this->getEntityManager()->getConnection()->query($Query)-> fetchAll(\PDO::FETCH_ASSOC);
