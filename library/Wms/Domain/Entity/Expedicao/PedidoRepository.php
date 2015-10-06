d0a9be5b1b7844dccda71248d8e94f7c8d5ae651
Lucas Chinelate <lucaschinelate@hotmail.com>
2015-10-06 09:46:48 -0300

    Correcao WebService

--- library/Wms/Domain/Entity/Expedicao/PedidoRepository.php
+++ library/Wms/Domain/Entity/Expedicao/PedidoRepository.php
@@ -258,7 +258,7 @@ class PedidoRepository extends EntityRepository
             $this->_em->remove($ondaRessuprimentoPedidoEn);
         }
 
-        $andamentoRepo->save("Pedido removido da expedição via WebService", $pedidoEntity->getCarga()->getExpedicao(),false,true,null,null,true);
+        $andamentoRepo->save("Pedido ". $pedidoEntity->getId() ."  removido da expedição " .  $pedidoEntity->getCarga()->getExpedicao()->getId() . "via WebService", $pedidoEntity->getCarga()->getExpedicao(),false,true,null,null,true);
 
         $this->_em->remove($pedidoEntity);
         $this->_em->flush();
