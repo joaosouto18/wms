35906e4db0977da094803b0076252418d1284006
Rodrigo Dantley <rodrigodantley@gmail.com>
2015-10-13 11:22:53 -0300

    acertar pesquisa na tela de expedicao

--- public/wms/controllers/expedicao_controller.js
+++ public/wms/controllers/expedicao_controller.js
@@ -93,16 +93,6 @@ $.Controller.extend('Wms.Controllers.Expedicao',
                     }
                 });
             });
-
-            $("#filtro-expedicao-mercadoria-form #submit").click(function() {
-                var url = location.href;
-                console.log(url.indexOf("expedicao/index"));
-                if (url.indexOf("expedicao/index") == 19) {
-                    console.log(url);
-                    $("#filtro-expedicao-mercadoria-form").attr('action', '/expedicao/index');
-                }
-            });
-
         }
 
 
