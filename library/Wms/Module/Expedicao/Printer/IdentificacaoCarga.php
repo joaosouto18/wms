17c6e67ebef6fb39d73efc88740666f12c55acbb
Lucas Chinelate <lucaschinelate@hotmail.com>
2015-10-15 14:08:49 -0300

    Layout Etiqueta Dalmo

--- library/Wms/Module/Expedicao/Printer/IdentificacaoCarga.php
+++ library/Wms/Module/Expedicao/Printer/IdentificacaoCarga.php
@@ -29,24 +29,35 @@ class IdentificacaoCarga extends Pdf
 
         $this->AddPage();
         $this->SetFont('Arial',  "B", 60);
-        $this->Cell(115, 30, utf8_decode("Expedição: "),0,0);
+        $this->Cell(115, 22, utf8_decode("Expedição: "),0,0);
         $this->SetFont('Arial',  null, 60);
-        $this->Cell(20, 30, utf8_decode($idExpedicao),0,1);
+        $this->Cell(20,  22, utf8_decode($idExpedicao),0,1);
         $this->SetFont('Arial',  "B", 60);
-        $this->Cell(65, 30, utf8_decode("Placa: "),0,0);
+        $this->Cell(65,  22, utf8_decode("Placa: "),0,0);
         $this->SetFont('Arial',  null, 60);
-        $this->Cell(20, 30, utf8_decode($codCargaExterno),0,1);
+        $this->Cell(20,  22, utf8_decode($codCargaExterno),0,1);
         $this->SetFont('Arial',  "B", 60);
-        $this->Cell(70, 30, utf8_decode("Carga: ") ,0,0);
+        $this->Cell(70,  22, utf8_decode("Carga: ") ,0,0);
         $this->SetFont('Arial',  null, 60);
-        $this->Cell(20, 30, utf8_decode($cargaEn->getPlacaCarga()),0,1);
+        $this->Cell(20,  22, utf8_decode($cargaEn->getPlacaCarga()),0,1);
 
+        $y = $this->GetY();
+        $x = $this->GetX();
 
+        $this->SetX(20);
+        $this->SetY(50);
+        $this->Cell(210,1,"",0,0);
+        $this->SetFont('Arial',  null, 160);
+        $this->Cell(1,1,$cargaEn->getSequencia(),0,0);
 
+        $this->SetX($x);
+        $this->SetY($y);
+        
         //$this->Line(0,$this->GetY(),500,$this->GetY());
         $this->Cell(20,20,"",0,1);
         $this->SetFont('Arial',  null, 80);
         $this->MultiCell("280","25",utf8_decode(implode($arrayItinerario,",")),0,"C");
+
 
         $this->Output('IdentificaçãoCarga.pdf','D');
 
