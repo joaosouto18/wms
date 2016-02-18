6f71fe97a5bad8b3966c79f1488a02f50fc07251
Rodrigo Dantley <rodrigodantley@gmail.com>
2016-02-18 13:37:57 -0200

    acerto de webservice de importação de nota fiscal

--- library/Wms/WebService/NotaFiscal.php
+++ library/Wms/WebService/NotaFiscal.php
@@ -211,8 +211,6 @@ class Wms_WebService_NotaFiscal extends Wms_WebService
     {
         $em = $this->__getDoctrineContainer()->getEntityManager();
         try{
-            var_dump($itens[0]['idProduto']);
-            exit;
             $em->beginTransaction();
 
             $idFornecedor = trim($idFornecedor);
