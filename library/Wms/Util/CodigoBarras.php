6f1999909b10cf9508fd1b178b581ea5bb2fb9c7
Rodrigo Dantley <rodrigodantley@gmail.com>
2016-03-18 17:11:04 -0300

    correção de bug ao imprimir UMA

--- library/Wms/Util/CodigoBarras.php
+++ library/Wms/Util/CodigoBarras.php
@@ -615,10 +615,10 @@ class CodigoBarras
         }
         ImageString($img,4,$lw*95+17,$hi-5,$code[11],$fg);
 
-        self::verificarDiretorioExistente(APPLICATION_PATH . '/../data/CodigoBarras/');
+        self::verificarDiretorioExistente('/data/CodigoBarras/');
 
-        ImagePNG($img, APPLICATION_PATH . '/../data/CodigoBarras/' . $nomeArquivo);
+        ImagePNG($img, 'data/CodigoBarras/' . $nomeArquivo);
 
-        return APPLICATION_PATH . '/../data/CodigoBarras/' . $nomeArquivo;
+        return 'data/CodigoBarras/' . $nomeArquivo;
     }
 }
