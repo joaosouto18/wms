9b4f12a746c19c1b9c5c27eda1599424397e3dde
Rodrigo Dantley <rodrigodantley@gmail.com>
2016-04-19 11:24:01 -0300

    acerto de cubagem

--- library/Wms/Module/Expedicao/Report/Produtos.php
+++ library/Wms/Module/Expedicao/Report/Produtos.php
@@ -208,7 +208,7 @@ class Produtos extends Pdf
                 $this->AddPage();
             }
 
-            if (($prodAnterior != $produto->getCodProduto()) OR ($gradeAnterior != $produto->getGrade()) || $reentregaAnterior != $produto['codReentrega']) {
+            if (($prodAnterior != $produto->getCodProduto()) OR ($gradeAnterior != $produto->getGrade())) {
                 $embalagemAnterior = null;
                 $arrayCargas = array();
                 $arrayQtd = array();
@@ -265,7 +265,7 @@ class Produtos extends Pdf
             };
 
             $prodAnterior = $produto->getCodProduto();
-            $reentregaAnterior = $produto['codReentrega'];
+//            $reentregaAnterior = $produto['codReentrega'];
             $gradeAnterior = $produto->getGrade();
             $seqQuebra = $produto->getSeqQuebra();
         }
