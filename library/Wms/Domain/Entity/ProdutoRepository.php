69f8c81d8951a0a1d344d13585c3abf9d0883cf8
Lucas Chinelate <lucaschinelate@hotmail.com>
2016-04-25 10:05:36 -0300

    Enviando o codigo de barras do produto para a simonetti

--- library/Wms/Domain/Entity/ProdutoRepository.php
+++ library/Wms/Domain/Entity/ProdutoRepository.php
@@ -801,7 +801,7 @@ class ProdutoRepository extends EntityRepository implements ObjectRepository {
 
   private function enviaDadosLogisticosEmbalagem(ProdutoEntity $produtoEntity) {
 	$dql = $this->getEntityManager()->createQueryBuilder()
-			->select('pe.descricao, pdl.altura, pdl.cubagem, pdl.largura, pdl.peso, pdl.profundidade, pe.quantidade ')
+			->select('pe.descricao, pdl.altura, pdl.cubagem, pdl.largura, pdl.peso, pdl.profundidade, pe.quantidade, pe.codigoBarras ')
 			->from('wms:Produto\DadoLogistico', 'pdl')
 			->innerJoin('wms:Produto\Embalagem', 'pe', 'WITH', 'pe.id = pdl.embalagem')
 			->where('pe.codProduto = ?1')
@@ -817,7 +817,7 @@ class ProdutoRepository extends EntityRepository implements ObjectRepository {
 	
 	if (empty($dadosLogisticosEmbalagens)) {
 	  $dql = $this->getEntityManager()->createQueryBuilder()
-			->select('pe.descricao, pdl.altura, pdl.cubagem, pdl.largura, pdl.peso, pdl.profundidade, pe.quantidade ')
+			->select('pe.descricao, pdl.altura, pdl.cubagem, pdl.largura, pdl.peso, pdl.profundidade, pe.quantidade, pe.codigoBarras ')
 			->from('wms:Produto\DadoLogistico', 'pdl')
 			->innerJoin('wms:Produto\Embalagem', 'pe', 'WITH', 'pe.id = pdl.embalagem')
 			->where('pe.codProduto = ?1')
@@ -847,6 +847,7 @@ class ProdutoRepository extends EntityRepository implements ObjectRepository {
 		  'peso' => $embalagem['peso'],
 		  'descricao' => $embalagem['descricao'],
           'quantidade' => $embalagem['quantidade'],
+		  'codigoBarras' =>$embalagem['codigoBarras']
 	  );
 
 	  $i++;
@@ -877,7 +878,8 @@ class ProdutoRepository extends EntityRepository implements ObjectRepository {
 		  'cubagem' => \Core\Util\Converter::brToEn($cubagem, 4),
 		  'peso' => \Core\Util\Converter::brToEn($peso, 3),
 		  'descricao' => $descricao,
-          'quantidade' => 1
+          'quantidade' => 1,
+		  'codigoBarras' => $codigoBarras
 	  );
 	  $i++;
 	}
