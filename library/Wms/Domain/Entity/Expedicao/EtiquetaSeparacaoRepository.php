b0c2d0e9f4e0c74c418e9cef3204bb06994b73f8
Rodrigo Dantley <rodrigodantley@gmail.com>
2016-05-10 16:31:45 -0300

    correção de bug na tela de conferencia cega

--- library/Wms/Domain/Entity/Expedicao/EtiquetaSeparacaoRepository.php
+++ library/Wms/Domain/Entity/Expedicao/EtiquetaSeparacaoRepository.php
@@ -1089,6 +1089,7 @@ class EtiquetaSeparacaoRepository extends EntityRepository
 
             //LINHA DE SEPARAÇÃO
             if ($quebra == "L") {
+                var_dump('abc');
                 var_dump($pedidoProduto);
                 var_dump($pedidoProduto->getLinhaSeparacao());
                 exit;
