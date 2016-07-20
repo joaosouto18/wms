607325de33edc9b2ae49acefcf625ea00b4244c7
morgcesar <tarcisiocms@outlook.com>
2016-07-20 10:50:21 -0300

    Remoção temporária do codigo de barra superior do mapa de separação

--- library/Wms/Module/Expedicao/Printer/MapaSeparacao.php
+++ library/Wms/Module/Expedicao/Printer/MapaSeparacao.php
@@ -73,6 +73,8 @@ class MapaSeparacao extends Pdf
                 }
             }
 
+            $imgCodBarras = @CodigoBarras::gerarNovo($this->idMapa);
+
             //Select Arial bold 8
             $this->SetFont('Arial','B',10);
             $this->Cell(200, 3, utf8_decode("MAPA DE SEPARAÇÃO " . $this->idMapa), 0, 1,"C");
@@ -86,6 +88,8 @@ class MapaSeparacao extends Pdf
             $this->Cell(20, 4, utf8_decode("QUEBRAS: "), 0, 0);
             $this->SetFont('Arial',null,10);
             $this->Cell(20, 4, utf8_decode($this->quebrasEtiqueta), 0, 1);
+
+            //$this->Image($imgCodBarras, 155, 18, 50);
 
 
             $this->Cell(20, 4, "", 0, 1);
@@ -127,7 +131,7 @@ class MapaSeparacao extends Pdf
             $this->Cell(20, 3, utf8_decode(date('d/m/Y')." às ".date('H:i')), 0, 1, "L");
 
             //$this->SetY(-92);
-            $this->Image(@CodigoBarras::gerarNovo($this->idMapa), 150, 280, 50);
+            $this->Image($imgCodBarras, 150, 280, 50);
 
         }
 
