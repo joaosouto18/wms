2d740b5e26a7ea039a0eb103cb40515205389ccf
Mateus Azevedo <mateus6@gmail.com>
2016-07-25 13:55:08 -0300

    mend

--- application/modules/mobile/controllers/Enderecamento/ReabastecimentoManualController.php
+++ application/modules/mobile/controllers/Enderecamento/ReabastecimentoManualController.php
@@ -158,8 +158,6 @@ class Mobile_Enderecamento_ReabastecimentoManualController extends Action
             $conexao = \Wms\Domain\EntityRepository::conexaoViewERP();
             $query = "select PRECO from FN_GET_PROD_IMPERIUM where CODPROD = $codProduto";
             $precoResult = \Wms\Domain\EntityRepository::nativeQuery($query, 'all', $conexao);
-            var_dump($precoResult);
-            exit;
             if (!empty($precoResult)) {
                 $preco = $precoResult[0]['PRECO'];
             }
