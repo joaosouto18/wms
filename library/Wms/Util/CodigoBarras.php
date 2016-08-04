e6bfd405b2f6de54090f92356490e81bd7bcdacd
Lucas Chinelate <lucaschinelate@hotmail.com>
2016-08-04 11:15:22 -0300

    Teste

--- library/Wms/Util/CodigoBarras.php
+++ library/Wms/Util/CodigoBarras.php
@@ -596,10 +596,11 @@ class CodigoBarras
             $bars.=$Rencode[$code[$x]];
         }
         $bars.=$ends;
-        var_dump("Aqui dentro 5");exit;
 
         /* Generate the Barcode Image */
         $img = ImageCreate($lw*95+30,$hi+30);
+        var_dump("Aqui dentro 5");exit;
+
         $fg = ImageColorAllocate($img, 0, 0, 0);
         $bg = ImageColorAllocate($img, 255, 255, 255);
         ImageFilledRectangle($img, 0, 0, $lw*95+30, $hi+30, $bg);
