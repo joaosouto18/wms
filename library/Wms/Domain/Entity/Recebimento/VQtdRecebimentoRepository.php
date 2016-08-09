c1934ac5513a9941fc87cf1ff2af1eadda64a7b3
Rodrigo Dantley <rodrigodantley@gmail.com>
2016-08-09 10:47:38 -0300

    correcao de quantidade total de recebimento de produto ao somar o enderecamento

--- library/Wms/Domain/Entity/Recebimento/VQtdRecebimentoRepository.php
+++ library/Wms/Domain/Entity/Recebimento/VQtdRecebimentoRepository.php
@@ -5,4 +5,15 @@ use Doctrine\ORM\EntityRepository;
 
 class VQtdRecebimentoRepository extends EntityRepository
 {
+    public function getQtdByRecebimento($idRecebimento,$idProduto,$grade)
+    {
+        $sql = $this->getEntityManager()->createQueryBuilder()
+            ->select('SUM(v.qtd) qtd')
+            ->from('wms:Recebimento\VQtdRecebimento','v')
+            ->where("v.codRecebimento = $idRecebimento")
+            ->andWhere("v.codProduto = '$idProduto'")
+            ->andWhere("v.grade = '$grade'");
+
+        return $sql->getQuery()->getArrayResult();
+    }
 }
