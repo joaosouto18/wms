5386ce3eb0d9566c9c84acec97402027fc1c8e7e
morgcesar <tarcisiocms@outlook.com>
2016-08-10 10:32:29 -0300

    Parametro carrinho separacao

---
+++ docs/DB/v4.0.0/carrinhoSeparacao.sql
@@ -0,0 +1,2 @@
+insert into parametro (COD_PARAMETRO, COD_CONTEXTO_PARAMETRO, dsc_parametro, DSC_titulo_parametro, ind_parametro_sistema, cod_tipo_atributo, dsc_valor_parametro)
+values (sq_parametro_01.nextval,(SELECT COD_CONTEXTO_PARAMETRO FROM CONTEXTO_PARAMETRO WHERE DSC_CONTEXTO_PARAMETRO LIKE 'EXPEDICAO'),'IND_QTD_CAIXA_PC','Quantidade de caixas por carrinho na separação PC','S','A',12);
