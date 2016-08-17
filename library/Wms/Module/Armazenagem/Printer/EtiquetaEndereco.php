817b147090885a8f36cfc9db05f8d271ddcab60a
Rodrigo Dantley <rodrigodantley@gmail.com>
2016-08-17 13:34:37 -0300

    correcao etiqueta endereço

--- library/Wms/Module/Armazenagem/Printer/EtiquetaEndereco.php
+++ library/Wms/Module/Armazenagem/Printer/EtiquetaEndereco.php
@@ -147,11 +147,11 @@ class EtiquetaEndereco extends Pdf
         $this->Cell(0,0," ",0,1);
 
         $this->SetFont('Arial', 'B', 18);
-        $this->Cell(148.4,14,$dscProduto,0,1);
+        $this->Cell(148.5,14,$dscProduto,0,1);
 
         if($dscProduto == "") {
             $this->SetFont('Arial', 'B', 18);
-            $this->Cell(148.5,14," Rua      Prédio     Nível    Apto.",0,1);
+            $this->Cell(148.4,14," Rua      Prédio     Nível    Apto.",0,1);
         }
 
         $posY = $this->GetY() - 3;
