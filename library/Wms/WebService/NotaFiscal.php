49c6e3210781dd496a123707437463448178ad5d
Rodrigo Dantley <rodrigodantley@gmail.com>
2016-08-23 10:34:49 -0300

    alteracao de metodo para teste

--- library/Wms/WebService/NotaFiscal.php
+++ library/Wms/WebService/NotaFiscal.php
@@ -9,7 +9,7 @@ class Item {
     public $idProduto;
     /** @var string */
     public $grade;
-    /** @var string */
+    /** @var double */
     public $quantidade;
     /** @var double */
     public $peso;
