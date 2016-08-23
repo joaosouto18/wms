9af0a129439b9bbdff8f8039f9eb09ec44096ac4
Rodrigo Dantley <rodrigodantley@gmail.com>
2016-08-23 18:18:36 -0300

    alteracao de variavel

--- library/Wms/WebService/NotaFiscal.php
+++ library/Wms/WebService/NotaFiscal.php
@@ -255,9 +255,8 @@ class Wms_WebService_NotaFiscal extends Wms_WebService
                     $itemWs['grade'] = trim($itemNf->grade);
                     $itemWs['quantidade'] = trim($itemNf->quantidade);
                     $itemWs['peso'] = trim(str_replace(',','.',$itemNf->peso));
-                    if (trim(is_null($itemNf->peso) || !isset($itemNf->peso) || empty($itemNf->peso) || $itemNf->peso == 0)) {
+                    if (trim(is_null($itemNf->peso) || !isset($itemNf->peso) || empty($itemNf->peso) || $itemNf->peso == 0))
                         $itemWs['peso'] = trim($itemNf->quantidade);
-                    }
 
                     $itensNf[] = $itemWs;
                 }
@@ -540,7 +539,7 @@ class Wms_WebService_NotaFiscal extends Wms_WebService
                     $itemWs['grade'] = trim($itemNf['grade']);
                     $itemWs['quantidade'] = trim($itemNf['quantidade']);
                     $itemWs['peso'] = trim(str_replace(',','.',$itemNf['peso']));
-                    if (is_null(trim($itemNf['peso'])) || empty(trim($itemNf['peso'])) || trim($itemNf['peso']) == 0 || $itemNf['peso'] == '') {
+                    if (is_null($itemNf['peso']) || strlen(trim($itemNf['peso'])) == 0) {
                         $itemWs['peso'] = trim(str_replace(',','.',$itemNf['quantidade']));
                     }
 
