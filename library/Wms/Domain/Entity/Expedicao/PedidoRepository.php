cf656283819902be4ee0356cd115cc64769bf2cb
morgcesar <tarcisiocms@outlook.com>
2016-09-16 12:17:10 -0300

    Correção de corte para clientes que não tem itinerario

--- library/Wms/Domain/Entity/Expedicao/PedidoRepository.php
+++ library/Wms/Domain/Entity/Expedicao/PedidoRepository.php
@@ -416,7 +416,7 @@ class PedidoRepository extends EntityRepository
     public function getPedidoByExpedicao($idExpedicao)
     {
         $sql = $this->getEntityManager()->createQueryBuilder()
-            ->select('p.id, pe.nome cliente, NVL(i.descricao,\'PADRAO\')')
+            ->select('p.id, pe.nome cliente, NVL(i.descricao,\'PADRAO\') as itinerario')
             ->from('wms:Expedicao\Pedido', 'p')
             ->innerJoin('wms:Pessoa','pe', 'WITH', 'pe.id = p.pessoa')
             ->leftJoin('wms:Expedicao\Itinerario', 'i', 'WITH', 'i.id = p.itinerario')
