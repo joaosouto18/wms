cd97a5b7e93ae34a153f80bfebcfa800e626bdca
tarcisiocms <tarcisiocms@outlook.com>
2016-10-18 20:50:51 -0200

    Conclusão do relatorio de produtividade com quebra de pagina

--- library/Wms/Module/Produtividade/Report/Apontamento.php
+++ library/Wms/Module/Produtividade/Report/Apontamento.php
@@ -12,11 +12,49 @@ class Apontamento extends Pdf
     private $startEndRowGroupH = 8;
     private $lineH = 6;
     private $body;
-    
-    private function startPage($orientacao, $dataInicio, $dataFim)
+    private $offsetHead = 10;
+    private $offsetListW = 12.5;
+    private $colIndexHeadW = 77.5;
+    private $colIndexW = 75;
+    private $colProdutoW = 30;
+    private $colCubagemW= 25;
+    private $colPesoW = 19;
+    private $colVolumesW = 22;
+    private $colPaletesW = 20;
+    private $dataInicio;
+    private $dataFim;
+    private $orientacao;
+    private $startY = 27;
+
+    public function generatePDF($params)
+    {
+        \Zend_Layout::getMvcInstance()->disableLayout(true);
+        \Zend_Controller_Front::getInstance()->setParam('noViewRenderer', true);
+
+        $this->marginLeft = 7;
+        $this->marginRight = $this->GetPageWidth() - $this->marginLeft;
+        $this->body = $this->pageW - (2 * $this->marginLeft);
+
+        $this->dataInicio = $params['dataInicio'];
+        $this->dataFim = $params['dataFim'];
+        $this->orientacao = $params['orientacao'];
+
+        $startY = self::startPage();
+
+        foreach($params['rows'] as $index => $rows){
+            $lastY = self::addGroup($params['orientacao'], $index, $rows, $startY);
+            $startY = ($lastY + 5);
+        }
+
+        self::Output('Relatorio de Produtividade por '.$params['orientacao'].'.pdf','D');
+    }
+
+    private function startPage()
     {
         $marginL = $this->marginLeft;
         $marginR = $this->marginRight;
+        $orientacao = $this->orientacao;
+
         $this->SetMargins($marginL,5);
         $this->AddPage();
         $this->SetLineWidth(1);
@@ -31,131 +69,108 @@ class Apontamento extends Pdf
         $this->Cell($this->body, 6, utf8_decode("Agrupado por $orientacao"),0,1,"C");
 
         $this->SetFont('Arial', '', 12);
+        $dataInicio = $this->dataInicio;
+        $dataFim = $this->dataFim;
         $this->Cell($this->body, 6, utf8_decode("Período de $dataInicio até $dataFim"),0,1,"C");
 
         $this->Image(APPLICATION_PATH .'\..\public\img\logo_quebec.jpg',160,6,40,12);
         $this->Line($marginL, 23, $marginR, 23);
+
+        return $this->startY;
     }
 
-    private function addProdutoRow($produto, $i)
-    { 
-        $lineH = $this->lineH;
-
-        // LINHA 1
-
-        $this->SetFont('Arial', 'B', 10);
-        $this->SetY($this->prodListY + ($i * (5 + (3 * $lineH))));
-        $this->SetFillColor(170);
-        $this->Cell(40, $lineH, utf8_decode("Produto: $produto[COD_PRODUTO]") ,1 ,0 ,'' , true);
-
-        $this->SetFont('Arial', 'B', 9);
-        $this->SetFillColor(220);
-        $cellWidth = 126;
-        $str = self::setStringByMaxWidth(utf8_decode("Descrição: $produto[DESCRICAO]"),$cellWidth);
-        $this->Cell($cellWidth, $lineH, $str ,1 ,0 ,'' , true);
-
-        $this->SetFont('Arial', 'B', 9);
-        $this->SetFillColor(190);
-        $this->Cell(30, $lineH, utf8_decode("Grade: $produto[GRADE]") ,1 ,1 ,'' , true);
-
-        // LINHA 2
-
-        $this->SetFont('Arial', 'B', 9);
-        $this->SetFillColor(220);
-        $cellWidth = 106;
-        $str = self::setStringByMaxWidth(utf8_decode("Linha de separação: $produto[LINHA_SEPARACAO]"), $cellWidth);
-        $this->Cell($cellWidth, $lineH, $str ,1 ,0 ,'' , true);
-
-        $this->SetFont('Arial', 'B', 9);
-        $this->SetFillColor(220);
-        $this->Cell(40, $lineH, utf8_decode("Endereço: $produto[ENDERECO]") ,1 ,0 ,'' , true);
-
-        $this->SetFont('Arial', 'B', 9);
-        $this->SetFillColor(220);
-        $strg = (!empty($produto['VALIDADE']))? $produto['VALIDADE'] : 'Sem Registro';
-        $this->Cell(50, $lineH, utf8_decode("Data de validade: $strg") ,1 ,1 ,'' , true);
-
-        //LINHA 3
-
-        $this->SetFont('Arial', 'B', 9);
-        $this->SetFillColor(220);
-        $cellWidth = 120;
-        $str = self::setStringByMaxWidth(utf8_decode("Fornecedor: $produto[FORNECEDOR]"), $cellWidth);
-        $this->Cell($cellWidth, $lineH, $str ,1 ,0 ,'' , true);
-
-        $this->SetFont('Arial', 'B', 9);
-        $this->SetFillColor(220);
-        $this->Cell(40, $lineH, utf8_decode("Qtd em estoque: $produto[QTD]") ,1 ,0 ,'' , true);
-
-        $this->SetFont('Arial', 'B', 9);
-        $this->SetFillColor(175);
-
-        $dt = date_create_from_format('d/m/Y', $produto['VALIDADE']) ;
-        $now = date_create_from_format('d/m/Y', date('d/m/Y'));
-        if (!empty($produto['VALIDADE']) && ($dt <= $now)){
-            $status = "VENCIDO";
-        } else if (!empty($produto['VALIDADE']) && ($dt > $now)) {
-            $status = "À VENCER";
-        } else {
-            $status = 'N/D';
-        }
-
-        $this->Cell(36, $lineH, utf8_decode("STATUS: $status") ,1 ,0 ,'C' , true);
-    }
-
-    public function Footer()
+    private function addGroup($orientacao, $groupIndex, $rowsGroup, $startY)
     {
-        $this->SetFont('Arial','',9);
-        $this->SetY(-20);
-        $this->Cell(176, 15, utf8_decode("Relatório gerado em ".date('d/m/Y')." às ".date('H:i:s')), 0, 0, "L");
-        $this->Cell(20, 15, utf8_decode('Página ').$this->PageNo(), 0, 1, 'R');
-    }
-
-    public function generatePDF($params)
-    {
-        \Zend_Layout::getMvcInstance()->disableLayout(true);
-        \Zend_Controller_Front::getInstance()->setParam('noViewRenderer', true);
-
-        $this->marginLeft = 7;
-        $this->marginRight = $this->GetPageWidth() - $this->marginLeft;
-        $this->body = $this->pageW - (2 * $this->marginLeft);
-
-        $sergH = $this->startEndRowGroupH;
-        $lineH = $this->lineH;
-        $pgH = $this->GetPageHeight();
-        $startY = 27;
-
-        self::startPage($params['orientacao'], $params['dataInicio'], $params['dataFim']);
-
-        $pgHeightRestante = $pgH;
-
-        foreach($params['rows'] as $index => $rows){
-            $groupH = (count($rows) * $lineH) + ($sergH * 3);
-            if ($groupH < $pgHeightRestante) {
-                self::addFullGroup($params['orientacao'], $index, $rows, $startY);
-            }
-            $startY += ($groupH + 5);
-            $pgHeightRestante -= $groupH;
-
-        }
-
-        self::Output('Relatorio de Produtividade por '.$params['orientacao'].'.pdf','D');
-    }
-
-    private function addFullGroup($orientacao, $groupIndex, $rowsGroup, $startY)
-    {
-
         $keyRow = ($orientacao == 'Atividade')? 'NOM_PESSOA' : 'DSC_ATIVIDADE';
         $headGroup = ($orientacao == 'Atividade')? 'Funcionário' : 'Atividade';
+        $lineH = $this->lineH;
+        $marginL = $this->marginLeft;
+        $marginR = $this->marginRight;
+
+        $groupHeadH = 16;
+        $groupEndH = 7;
+
+        $footerH = 18;
+        $posicaoAtual = $this->GetY();
+        $pageH = (int) $this->GetPageHeight();
+        $heightRestante = $pageH - $posicaoAtual - $footerH;
+
+        if ($groupHeadH > $heightRestante) {
+            $startY = self::startPage();
+        }
+
+        self::startGroup($startY, $groupIndex, $headGroup);
+
+        $this->SetFont('Arial','',9);
+        $startYGroup = $startY + $groupHeadH;
+        $this->SetY($startYGroup);
+
+        $i = 1;
         $tItens = 0;
         $tVolumes = 0;
         $tCubagem = 0;
         $tPeso = 0;
         $tPalete = 0;
-        $lineH = $this->lineH;
+        foreach ($rowsGroup as $key => $row) {
+
+            $qtdProduto = (!empty($row['QTD_PRODUTOS']))?$row['QTD_PRODUTOS']:0;
+            $tItens += $qtdProduto;
+
+            $qtdCubagem = (!empty($row['QTD_CUBAGEM']))?$row['QTD_CUBAGEM']:0;
+            $tCubagem += $qtdCubagem;
+
+            $qtdPeso = (!empty($row['QTD_PESO']))?$row['QTD_PESO']:0;
+            $tPeso += $qtdPeso;
+
+            $qtdVolumes = (!empty($row['QTD_VOLUMES']))?$row['QTD_VOLUMES']:0;
+            $tVolumes += $qtdVolumes;
+
+            $qtdPaletes = (!empty($row['QTD_PALETES']))?$row['QTD_PALETES']:0;
+            $tPalete += $qtdPaletes;
+
+            $posicaoAtual = $this->GetY();
+            $heightRestante = $pageH - $posicaoAtual - $footerH - ($lineH + $groupEndH);
+
+            if ($heightRestante < 0) {
+                $startY = self::startPage();
+                self::startGroup($startY, $groupIndex, $headGroup);
+                $this->SetFont('Arial','',9);
+                $startYGroup = $startY + $groupHeadH;
+                $this->SetY($startYGroup);
+                $i = 1;
+            }
+
+            self::addListRow($lineH, $row[$keyRow], $qtdProduto, $qtdCubagem, $qtdPeso, $qtdVolumes, $qtdPaletes);
+
+            $endLineY = $startYGroup + ($lineH * $i);
+            $this->Line($marginL + 10, $endLineY, $marginR, $endLineY);
+            $i++;
+        }
+
+        self::endGroup($lineH, $tItens, $tCubagem, $tPeso, $tVolumes, $tPalete, $i, $startYGroup, $marginL, $marginR);
+
+        return $this->GetY();
+    }
+
+    private function addListRow($lineH, $index, $qtdProduto, $qtdCubagem, $qtdPeso, $qtdVolumes, $qtdPaletes)
+    {
+        $this->Cell($this->offsetListW, $lineH);
+        $cellWidth = $this->colIndexW;
+        $str = self::setStringByMaxWidth(utf8_decode($index),$cellWidth);
+        $this->Cell($cellWidth, $lineH, $str,0,0);
+        $this->Cell($this->colProdutoW, $lineH, $qtdProduto,0,0);
+        $this->Cell($this->colCubagemW, $lineH, $qtdCubagem,0,0);
+        $this->Cell($this->colPesoW, $lineH, $qtdPeso,0,0);
+        $this->Cell($this->colVolumesW, $lineH, $qtdVolumes,0,0);
+        $this->Cell($this->colPaletesW, $lineH, $qtdPaletes,0,1);
+    }
+
+    private function startGroup($startY, $groupIndex, $headGroup)
+    {
         $sergH = $this->startEndRowGroupH;
         $marginL = $this->marginLeft;
         $marginR = $this->marginRight;
+        $orientacao = $this->orientacao;
 
         $this->SetY($startY);
         $this->SetFont('Arial', '',10);
@@ -164,70 +179,39 @@ class Apontamento extends Pdf
         $this->Cell(80, $sergH, utf8_decode($groupIndex), 0, 1);
 
         $this->SetLineWidth(0.5);
-
         $this->Line($marginL, $startY + 1 + $sergH, $marginR,  $startY + 1 + $sergH);
 
         $this->SetY($startY + 2 + $sergH);
         $this->SetFont('Arial', 'B',9);
-        $this->Cell(10);
-        $this->Cell(65, $sergH, utf8_decode($headGroup));
-        $this->Cell(30, $sergH, utf8_decode("Qtde produtos"));
-        $this->Cell(35, $sergH, utf8_decode("Cubagem"));
-        $this->Cell(25, $sergH, utf8_decode("Peso"));
-        $this->Cell(20, $sergH, utf8_decode("Volumes"));
-        $this->Cell(20, $sergH, utf8_decode("Paletes"));
+        $this->Cell($this->offsetHead);
+        $this->Cell($this->colIndexHeadW, $sergH, utf8_decode($headGroup));
+        $this->Cell($this->colProdutoW, $sergH, utf8_decode("Qtde produtos"));
+        $this->Cell($this->colCubagemW, $sergH, utf8_decode("Cubagem"));
+        $this->Cell($this->colPesoW, $sergH, utf8_decode("Peso"));
+        $this->Cell($this->colVolumesW, $sergH, utf8_decode("Volumes"));
+        $this->Cell($this->colPaletesW, $sergH, utf8_decode("Paletes"),0,1);
+    }
 
-        $this->SetFont('Arial','',9);
-        $startYGroup = $startY + 16;
-        $this->SetY($startYGroup);
-
-        $i = 1;
-        foreach ($rowsGroup as $row) {
-            $this->Cell(15, $lineH);
-            $cellWidth = 65;
-            $str = self::setStringByMaxWidth(utf8_decode($row[$keyRow]),$cellWidth);
-            $this->Cell($cellWidth, $lineH, $str,0,0);
-
-            $qtd = (!empty($row['QTD_PRODUTOS']))?$row['QTD_PRODUTOS']:0;
-            $tItens += $qtd;
-            $this->Cell(30, $lineH, $qtd,0,0);
-
-            $qtd = (!empty($row['QTD_CUBAGEM']))?$row['QTD_CUBAGEM']:0;
-            $tCubagem += $qtd;
-            $this->Cell(35, $lineH, $row['QTD_CUBAGEM'],0,0);
-
-            $qtd = (!empty($row['QTD_PESO']))?$row['QTD_PESO']:0;
-            $tPeso += $qtd;
-            $this->Cell(20, $lineH, $row['QTD_PESO'],0,0);
-
-            $qtd = (!empty($row['QTD_VOLUMES']))?$row['QTD_VOLUMES']:0;
-            $tVolumes += $qtd;
-            $this->Cell(25, $lineH, $row['QTD_VOLUMES'],0,0);
-
-            $qtd = (!empty($row['QTD_PALETES']))?$row['QTD_PALETES']:0;
-            $tPalete += $qtd;
-            $this->Cell(20, $lineH, $row['QTD_PALETES'],0,1);
-
-
-            $endLineY = $startYGroup + ($lineH * $i);
-            $this->Line($marginL + 10, $endLineY, $marginR, $endLineY);
-            $i++;
-        }
+    private function endGroup($lineH, $tItens, $tCubagem, $tPeso, $tVolumes, $tPalete, $i, $startYGroup, $marginL, $marginR)
+    {
         $this->SetFont('Arial','B',11);
-        $this->Cell(15, $lineH);
-        $this->Cell(65, $lineH, 'TOTAL',0,0);
-        $this->Cell(30, $lineH, $tItens,0,0);
-        $this->Cell(35, $lineH, $tCubagem,0,0);
-        $this->Cell(20, $lineH, $tPeso,0,0);
-        $this->Cell(25, $lineH, $tVolumes,0,0);
-        $this->Cell(20, $lineH, $tPalete,0,1);
+        $this->Cell($this->offsetListW, $lineH);
+        $this->Cell($this->colIndexW, $lineH, 'TOTAL',0,0);
+        $this->Cell($this->colProdutoW, $lineH, $tItens,0,0);
+        $this->Cell($this->colCubagemW, $lineH, $tCubagem,0,0);
+        $this->Cell($this->colPesoW, $lineH, $tPeso,0,0);
+        $this->Cell($this->colVolumesW, $lineH, $tVolumes,0,0);
+        $this->Cell($this->colPaletesW, $lineH, $tPalete,0,1);
 
         $endGroupY = $startYGroup + ($lineH * $i) ;
         $this->Line($marginL, $endGroupY, $marginR, $endGroupY);
     }
 
-    private function addPartialGroup($orientacao, $groupIndex, $rowsGroup, $y, $endRow)
+    public function Footer()
     {
-
+        $this->SetFont('Arial','',9);
+        $this->SetY(-20);
+        $this->Cell(176, 15, utf8_decode("Relatório gerado em ".date('d/m/Y')." às ".date('H:i:s')), 0, 0, "L");
+        $this->Cell(20, 15, utf8_decode('Página ').$this->PageNo(), 0, 1, 'R');
     }
 }
