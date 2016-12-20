1e0170cbfe362535984e549a41e118f20f4aad10
Rodrigo Dantley <rodrigodantley@gmail.com>
2016-12-20 18:16:15 -0200

    correcao de number format

--- library/Wms/Domain/Entity/Expedicao/MapaSeparacaoRepository.php
+++ library/Wms/Domain/Entity/Expedicao/MapaSeparacaoRepository.php
@@ -430,7 +430,7 @@ class MapaSeparacaoRepository extends EntityRepository
         $qtdDigitada = (float)$qtdEmbalagem * (float)number_format($quantidade,2,'.','');
         $qtdBanco    = (float)$qtdConferida + (float)$qtdCortada;
         $qtdMapa     = (float)$qtdMapa;
-        if (($qtdBanco + $qtdDigitada) > $qtdMapa) {
+        if ((number_format($qtdBanco,3,'.','') + number_format($qtdDigitada,'.','')) > number_format($qtdMapa,'.','')) {
             throw new \Exception("Quantidade informada(".$qtdEmbalagem * $quantidade.") + $qtdConferida excede a quantidade solicitada no mapa para esse cliente!");
         }
 
