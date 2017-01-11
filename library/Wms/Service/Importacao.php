7bf1e38c06cdf8eb0a835216adca831291ab105b
Lucas Chinelate <lucaschinelate@hotmail.com>
2017-01-11 14:41:37 -0200

    Persistindo Embalagens

--- library/Wms/Service/Importacao.php
+++ library/Wms/Service/Importacao.php
@@ -757,6 +757,24 @@ class Importacao
         return true;
     }
 
+    private function verificaCodigoBarrasDuplicado($codBarras, $idProduto, $grade) {
+        $SQL = "SELECT P.COD_PRODUTO, P.DSC_PRODUTO
+                  FROM PRODUTO P
+                  LEFT JOIN PRODUTO_EMBALAGEM PE ON PE.COD_PRODUTO = P.COD_PRODUTO AND PE.DSC_GRADE = P.DSC_GRADE
+                  LEFT JOIN PRODUTO_VOLUME PV ON PV.COD_PRODUTO = P.COD_PRODUTO AND PV.DSC_GRADE = P.DSC_GRADE
+                 WHERE NVL(PE.COD_BARRAS, PV.COD_BARRAS) = '$codBarras'
+                   AND P.COD_PRODUTO <> '$idProduto'
+                   AND P.DSC_GRADE <> '$grade'";
+
+        $em = $this->__getDoctrineContainer()->getEntityManager();
+        $produtos =  $em->getConnection()->query($SQL)->fetchAll(\PDO::FETCH_ASSOC);
+        if (count($produtos) >0) {
+            $prod = $produtos[0];
+            throw new \Exception("A Embalagem " . $codBarras ." se encontra em uso no sistema para o produto " . $prod['COD_PRODUTO'] . "/" . $prod['DSC_PRODUTO']);
+        }
+        return true;
+    }
+
     public function saveProduto($em, $produto, $repositorios)
     {
         /** @var EntityManager $em */
