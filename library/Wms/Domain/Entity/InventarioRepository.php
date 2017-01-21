61604f965fd3f7078c701b2c0f4b9f984cd2cae9
Lucas Chinelate <lucaschinelate@hotmail.com>
2017-01-21 14:41:23 -0200

    Correção

--- library/Wms/Domain/Entity/InventarioRepository.php
+++ library/Wms/Domain/Entity/InventarioRepository.php
@@ -470,7 +470,7 @@ class InventarioRepository extends EntityRepository
         $params['qtd']          = $qtd;
         $params['volume']       = $estoqueEn->getProdutoVolume();
         $params['embalagem']    = 0;
-        $params['validade']      = $contagemEndEn->getValidade();
+        $params['validade']     = null;
         $params['tipo']         = 'I';
         $params['observacoes']  = 'Mov. correção inventário';
         $params['os']           = $osEn;
