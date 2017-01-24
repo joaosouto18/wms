369a0e2a1fdd172d95ea4a016408f4befd21bced
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-01-24 15:15:16 -0200

    imprimir codigo de barras do produto junto com o mapa de separacao

--- library/Wms/Module/Expedicao/Printer/MapaSeparacao.php
+++ library/Wms/Module/Expedicao/Printer/MapaSeparacao.php
@@ -8,6 +8,8 @@ use
     Wms\Domain\Entity\Expedicao;
 use Wms\Util\Barcode\Barcode;
 
+use Wms\Util\Barcode\eFPDF;
+
 class MapaSeparacao extends Pdf
 {
     private $idMapa;
@@ -821,6 +823,42 @@ class MapaSeparacao extends Pdf
             $em->persist($ExpedicaoEntity);
         }
 
+        /** @var \Wms\Domain\Entity\Expedicao\MapaSeparacaoProdutoRepository $mapaSeparacaoProdutoRepo */
+        $mapaSeparacaoProdutoRepo = $em->getRepository('wms:Expedicao\MapaSeparacaoProduto');
+        $produtos = $mapaSeparacaoProdutoRepo->getMapaProdutoByExpedicao($idExpedicao);
+
+        $this->AddPage();
+        $x = 150;
+        $y = 15;
+        $count = 1;
+        foreach ($produtos as $produto)
+        {
+            $height   = 8;
+            $angle    = 0;
+            $type     = 'code128';
+            $black    = '000000';
+
+            if($count > 12){
+                $this->AddPage();
+                $count = 1;
+                $y = 30;
+            }
+
+            $this->SetFont('Arial','',10);
+            $this->Cell(15, 20, $produto['id'], 0, 0);
+            $this->Cell(90, 20, substr($produto['descricao'],0,40), 0, 0);
+            $this->Cell(90, 20, $produto['unidadeMedida'], 0, 1);
+            //$this->Cell(20, 20, '', 0, 1);
+            //$this->Cell(20, 10, '', 0, 1,'C');
+
+            $data = @CodigoBarras::gerarNovo($produto['codigoBarras']);
+            $this->Image($data, $x, $y, 50);
+//            $data = Barcode::fpdf($this,$black,$x,$y,$angle,$type,array('code'=>$produto['codigoBarras']),0.5,10);
+//            $len = $this->GetStringWidth($data['hri']);
+//            $this->Text(($x-$height) + (($height - $len)/2) + 3, $y + 8,$produto['codigoBarras']);
+            $y = $y + 20;
+            $count++;
+        }
         $this->Output('Mapa Separação-'.$idExpedicao.'.pdf','D');
 
         $em->flush();
