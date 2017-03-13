bbf7bfbd79ceeb0011fd44b7e6f52bfb943c3537
tarcisiocms <tarcisiocms@outlook.com>
2017-03-13 10:03:26 -0300

    Ticket 263

--- application/modules/mobile/controllers/ConsultaEnderecoController.php
+++ application/modules/mobile/controllers/ConsultaEnderecoController.php
@@ -1,57 +1,55 @@
 <?php
 use Wms\Controller\Action,
-    \Wms\Util\Endereco as EnderecoUtil,
-    Wms\Domain\Entity\Expedicao;
+    \Wms\Util\Endereco as EnderecoUtil;
 
 class Mobile_ConsultaEnderecoController extends Action
 {
     public function indexAction()
     {
         $codigoBarras = $this->_getParam('codigoBarras');
-        if (isset($codigoBarras) && !empty($codigoBarras)) {
-            $LeituraColetor = new \Wms\Service\Coletor();
-            $codigoBarras = $LeituraColetor->retiraDigitoIdentificador($codigoBarras);
+        if (!empty($codigoBarras)) {
+            try {
+                $LeituraColetor = new \Wms\Service\Coletor();
+                $codigoBarras = $LeituraColetor->retiraDigitoIdentificador($codigoBarras);
 
-            /** @var \Wms\Domain\Entity\Deposito\EnderecoRepository $enderecoRepo */
-            $enderecoRepo = $this->em->getRepository("wms:Deposito\Endereco");
-            $endereco = EnderecoUtil::formatar($codigoBarras);
-            /** @var \Wms\Domain\Entity\Deposito\Endereco $enderecoEn */
-            $enderecoEn = $enderecoRepo->findOneBy(array('descricao' => $endereco));
-            if (!isset($enderecoEn) || empty($enderecoEn)) {
-                $produtos = array(0 => array('COD_PRODUTO' => 'Endereço não encontrado', 'DSC_PRODUTO' => ''));
-                $this->_helper->json(array('produtos' => $produtos));
+                $usaGrade = $this->getSystemParameterValue('UTILIZA_GRADE');
+
+                /** @var \Wms\Domain\Entity\Deposito\EnderecoRepository $enderecoRepo */
+                $enderecoRepo = $this->em->getRepository("wms:Deposito\Endereco");
+                $endereco = EnderecoUtil::formatar($codigoBarras);
+
+                /** @var \Wms\Domain\Entity\Deposito\Endereco $enderecoEn */
+                $enderecoEn = $enderecoRepo->findOneBy(array('descricao' => $endereco));
+                if (!isset($enderecoEn) || empty($enderecoEn)) {
+                    throw new Exception("Endereço não encontrado");
+                }
+
+                /** @var \Wms\Domain\Entity\Enderecamento\EstoqueRepository $estoqueRepo */
+                $estoqueRepo = $this->getEntityManager()->getRepository('wms:Enderecamento\Estoque');
+                $itens = $estoqueRepo->findBy(array('depositoEndereco' => $enderecoEn));
+
+                if (empty($itens)) {
+                    throw new Exception("Não existe produto nesse endereço");
+                }
+
+                $result = array();
+                /** @var \Wms\Domain\Entity\Enderecamento\Estoque $item */
+                foreach ($itens as $item) {
+                    $produtoEn = $item->getProduto();
+                    $produto = array(
+                        'produto' => $produtoEn->getId(),
+                        'grade' => $produtoEn->getGrade(),
+                        'desc' => $produtoEn->getDescricao()
+                    );
+                    $result[] = $produto;
+                }
+
+                $this->_helper->json(array('status' => 'ok', 'result' => $result));
+
+            }catch (Exception $e){
+                $this->_helper->json(array('status'=> 'exception', 'msg' => $e->getMessage()));
             }
-
-            /** @var \Wms\Domain\Entity\Produto\EmbalagemRepository $embalagemRepo */
-            $embalagemRepo = $this->getEntityManager()->getRepository('wms:Produto\Embalagem');
-            $itens = $embalagemRepo->findBy(array('endereco' => $enderecoEn));
-
-            if (empty($itens)){
-                $volumeRepo = $this->em->getRepository('wms:Produto\Volume');
-                $itens = $volumeRepo->findBy(array('endereco' => $enderecoEn));
-            }
-
-            if (empty($itens)){
-                $produtos = array(0 => array('COD_PRODUTO' => 'Não Existe produto nesse endereço', 'DSC_PRODUTO' => ''));
-                $this->_helper->json(array('produtos' => $produtos));
-            }
-            $produtos = array();
-            /** @var \Wms\Domain\Entity\Produto\Embalagem|\Wms\Domain\Entity\Produto\Volume $item */
-            foreach ($itens as $item) {
-                $produtoEn = $item->getProduto();
-                $produto = array(
-                    'COD_PRODUTO' => $produtoEn->getId(),
-                    'DSC_GRADE' => $produtoEn->getGrade(),
-                    'DSC_PRODUTO' => $produtoEn->getDescricao()
-                );
-                $produtos[] = $produto;
-            }
-
-            $this->_helper->json(array('produtos' => $produtos));
-
         }
-
     }
-
 }
 
