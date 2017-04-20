8fe1a65de4a9db982e4f7cf1f06a22e017cf9779
Lucas Chinelate <lucaschinelate@hotmail.com>
2017-04-20 13:15:54 -0300

    Correção do Run Flush

--- library/Wms/Domain/Entity/Expedicao/ItinerarioRepository.php
+++ library/Wms/Domain/Entity/Expedicao/ItinerarioRepository.php
@@ -6,7 +6,7 @@ use Doctrine\ORM\EntityRepository;
 class ItinerarioRepository extends EntityRepository
 {
 
-    public function save($itinerario, $runFlush) {
+    public function save($itinerario, $runFlush = false) {
 
         $em = $this->getEntityManager();
 
