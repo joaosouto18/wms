36309351e4dfb9c9f74ff0de73551048dc5505c4
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-04-20 14:54:55 -0300

    correcao de listagem de enderecos para impressao

--- library/Wms/Domain/Entity/Deposito/EnderecoRepository.php
+++ library/Wms/Domain/Entity/Deposito/EnderecoRepository.php
@@ -877,7 +877,7 @@ class EnderecoRepository extends EntityRepository
         if (!empty ($params['predio'])) {
             $query = $query . " AND DEP.NUM_PREDIO >= " . $params['predio'];
         }
-        if (!empty ($params['nivel'])) {
+        if (isset($params['nivel']) && !is_null($params['nivel'])) {
             $query = $query . " AND DEP.NUM_NIVEL >= " . $params['nivel'];
         }
         if (!empty ($params['apartamento'])) {
@@ -890,7 +890,7 @@ class EnderecoRepository extends EntityRepository
         if (!empty ($params['prediofinal'])) {
             $query = $query . " AND DEP.NUM_PREDIO <= " . $params['prediofinal'];
         }
-        if (!empty ($params['nivelfinal'])) {
+        if (isset($params['nivelfinal']) && !is_null($params['nivelfinal'])) {
             $query = $query . " AND DEP.NUM_NIVEL <= " . $params['nivelfinal'];
         }
         if (!empty ($params['apartamentofinal'])) {
