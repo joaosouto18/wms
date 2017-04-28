8f8cd0d1928d82f8b006ff6854f3ae9cf656645a
Lucas Chinelate <lucaschinelate@hotmail.com>
2017-04-28 19:14:58 -0300

    Wilso

--- library/Wms/Domain/Entity/NotaFiscalRepository.php
+++ library/Wms/Domain/Entity/NotaFiscalRepository.php
@@ -867,9 +867,6 @@ class NotaFiscalRepository extends EntityRepository
 
             $statusEntity = $em->getReference('wms:Util\Sigla', NotaFiscalEntity::STATUS_INTEGRADA);
 
-            var_dump($dataEmissao);
-            var_dump(\DateTime::createFromFormat('d/m/Y', $dataEmissao));exit;
-
             //inserção de nova NF
             $notaFiscalEntity = new NotaFiscalEntity;
             $notaFiscalEntity->setNumero($numero);
