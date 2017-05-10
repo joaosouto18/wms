acfde52e58164b1885fdddae1d910a199d989374
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-05-10 19:40:22 -0300

    correcao de quebra de memoria em sequenciamento de carga

--- application/modules/expedicao/controllers/CarregamentoController.php
+++ application/modules/expedicao/controllers/CarregamentoController.php
@@ -7,6 +7,8 @@ class Expedicao_CarregamentoController extends Action
 
     public function indexAction()
     {
+        ini_set('max_execution_time', 3000);
+        ini_set('memory_limit', '-1');
         $form = new \Wms\Module\Expedicao\Form\Carregamento();
 
         $request = $this->getRequest();
