f2ce0cf4e67489cba7a102f16a2cb96504c1db05
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-05-18 17:30:14 -0300

    debug de integracao de pedidos

--- library/Wms/Domain/Entity/Expedicao/PedidoEnderecoRepository.php
+++ library/Wms/Domain/Entity/Expedicao/PedidoEnderecoRepository.php
@@ -10,7 +10,7 @@ class PedidoEnderecoRepository extends EntityRepository
     public function save($pedidoEntity, $pedidoCliente) {
 
         $em = $this->getEntityManager();
-//        $em->beginTransaction();
+        $em->beginTransaction();
 
         try {
             // pegar referência do pedido
@@ -32,10 +32,10 @@ class PedidoEnderecoRepository extends EntityRepository
             $enPedidoEndereco->setCep($pedidoCliente['cep']);
 
             $em->persist($enPedidoEndereco);
-//            $em->flush();
-//            $em->commit();
+            $em->flush();
+            $em->commit();
         } catch(\Exception $e) {
-//            $em->rollback();
+            $em->rollback();
             throw new \Exception($e->getMessage() . ' - ' .$e->getTraceAsString());
         }
 
