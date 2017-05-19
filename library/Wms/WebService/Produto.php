13d70001fa1d825154575c7bf7e3764118576df8
tarcisiocms <tarcisiocms@outlook.com>
2017-05-19 11:03:15 -0300

    Verificação de todas as entradas de cadastro de produto com peso variavel

--- library/Wms/WebService/Produto.php
+++ library/Wms/WebService/Produto.php
@@ -134,10 +134,11 @@ class Wms_WebService_Produto extends Wms_WebService {
      * @param string $idClasse ID da classe do produto
      * @param embalagem[] $embalagens Embalagens
      * @param string $referencia Código de Referencia do produto no fornecedor
+     * @param string $possuiPesoVariavel 'N' , 'S'
      * @throws Exception
      * @return boolean Se o produto foi inserido com sucesso ou não
      */
-    public function salvar($idProduto, $descricao, $grade, $idFabricante, $tipo, $idClasse, $embalagens, $referencia) {
+    public function salvar($idProduto, $descricao, $grade, $idFabricante, $tipo, $idClasse, $embalagens, $referencia, $possuiPesoVariavel) {
 
         $idProduto = trim ($idProduto);
         $descricao = trim ($descricao);
@@ -148,6 +149,9 @@ class Wms_WebService_Produto extends Wms_WebService {
         $idFabricante = trim ($idFabricante);
         $tipo = trim ($tipo);
         $idClasse = trim($idClasse);
+
+        if ($referencia === "?" || empty($referencia)) $referencia = "";
+        if ($possuiPesoVariavel === "?" || empty($possuiPesoVariavel)) $possuiPesoVariavel = "N";
 
         $service = $this->__getServiceLocator()->getService('Produto');
         $em = $this->__getDoctrineContainer()->getEntityManager();
@@ -185,7 +189,8 @@ class Wms_WebService_Produto extends Wms_WebService {
                 ->setGrade($grade)
                 ->setFabricante($fabricante)
                 ->setClasse($classe)
-                ->setReferencia($referencia);
+                ->setReferencia($referencia)
+                ->setPossuiPesoVariavel($possuiPesoVariavel);
 
             if ($produtoNovo == true) {
                 $produto
