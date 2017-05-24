36854a52cc9a54fd537e25caf9c3c75fbf3a05db
tarcisiocms <tarcisiocms@outlook.com>
2017-05-24 19:25:57 -0300

    Teste dump

--- library/Wms/Service/Importacao.php
+++ library/Wms/Service/Importacao.php
@@ -383,14 +383,11 @@ class Importacao
 
             var_dump("Achou a pessoa: " . $entityPessoa->getNome());
             try {
-
                 $entityFornecedor->setId($entityPessoa->getId());
                 $entityFornecedor->setIdExterno($fornecedor['idExterno']);
+                var_dump($entityFornecedor);
 
                 $em->persist($entityFornecedor);
-                $em->flush($entityFornecedor);
-                var_dump("persistiu");
-                var_dump($entityFornecedor);
                 return true;
             }catch (\Exception $e){
                 return $e->getMessage();
