5bfba40b5df45070ada2fa99e885a8fa85170402
Lucas Chinelate <lucaschinelate@hotmail.com>
2017-05-31 08:32:34 -0300

    Integracao

--- application/modules/importacao/controllers/GerenciamentoController.php
+++ application/modules/importacao/controllers/GerenciamentoController.php
@@ -1,21 +1,41 @@
 <?php
 
 use Wms\Module\Web\Controller\Action;
+use \Wms\Module\Web\Page;
 
 class Importacao_GerenciamentoController extends Action
 {
 
     public function indexAction()
     {
+
+        $request = $this->getRequest();
+        $params = $request->getParams();
+        $acao = $params['id'];
+
+        Page::configure(array(
+            'buttons' => array(
+                array(
+                    'label' => 'Buscar Registros no ERP',
+                    'cssClass' => 'btnSave',
+                    'urlParams' => array(
+                        'module' => 'importacao',
+                        'controller' => 'gerenciamento',
+                        'action' => 'index',
+                        'id' => $acao
+                    ),
+                    'tag' => 'a'
+                )
+            )
+        ));
+
+
         $em = $this->getEntityManager();
         try {
 //            $em->beginTransaction();
 
             /** @var \Wms\Domain\Entity\Integracao\AcaoIntegracaoRepository $acaoIntRepo */
             $acaoIntRepo = $this->getEntityManager()->getRepository('wms:Integracao\AcaoIntegracao');
-            $request = $this->getRequest();
-            $params = $request->getParams();
-            $acao = $params['id'];
             $acoesId = explode(",", $acao);
             $dataUltimaExecucao = $acaoIntRepo->findOneBy(array('id' => $acoesId[0]))->getDthUltimaExecucao();
             $dataUltimaExecucao = $dataUltimaExecucao->format('d/m/Y H:i:s');
