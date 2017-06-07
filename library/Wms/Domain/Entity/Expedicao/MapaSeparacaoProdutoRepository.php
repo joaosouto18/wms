974cb109075082d172bfe7653dda285c3ef38c58
Lucas Chinelate <lucaschinelate@hotmail.com>
2017-06-07 17:35:41 -0300

    regra de negocio para o corte

--- library/Wms/Domain/Entity/Expedicao/MapaSeparacaoProdutoRepository.php
+++ library/Wms/Domain/Entity/Expedicao/MapaSeparacaoProdutoRepository.php
@@ -9,6 +9,144 @@ use Wms\Domain\Entity\Expedicao;
 class MapaSeparacaoProdutoRepository extends EntityRepository
 {
 
+    public function efetivaCorteMapasERP($pedidosCortar, $produtosCortar) {
+
+        //* SE NÃO TIVER NENHUM PRODUTO PARA CORTAR, ENTÂO NAO PRECISO FAZER NENHUM CORTE EM NENHUM MAPA, RETORNO TRUE
+        if (count($produtosCortar) == 0) return true;
+
+        // VERIFICO TODOS OS PEDIDOS_PRODUTOS QUE TEM QUE SER CORTADOS
+        $SQLWherePP = "(";
+        foreach ($produtosCortar as $pp) {
+            if ($SQLWherePP != "(") {
+                $SQLWherePP .= " OR ";
+            }
+            $SQLWherePP .= "(PP.COD_PRODUTO = '" . $pp['codProduto'] . "' AND PP.DSC_GRADE = '" . $pp['grade'] ."')";
+        }
+        $SQLWherePP .= ")";
+        $SQL = " SELECT PP.COD_PEDIDO_PRODUTO, PP.QTD_CORTADA, PP.COD_PRODUTO, PP.DSC_GRADE, PP.COD_PEDIDO, P.DSC_PRODUTO
+                   FROM PEDIDO_PRODUTO PP
+                   LEFT JOIN PRODUTO P ON P.COD_PRODUTO = PP.COD_PRODUTO AND P.DSC_GRADE = PP.DSC_GRADE
+                  WHERE PP.COD_PEDIDO IN($pedidosCortar)
+                    AND $SQLWherePP
+                    AND QTD_CORTADA > 0";
+        $ppCortar = $this->getEntityManager()->getConnection()->query($SQL)->fetchAll(\PDO::FETCH_ASSOC);
+
+        foreach ($ppCortar as $pp) {
+            $qtdCortar = $pp['QTD_CORTADA'];
+            $codPP = $pp['COD_PEDIDO_PRODUTO'];
+            $codProduto = $pp['COD_PRODUTO'];
+            $dscGrade = $pp['DSC_GRADE'];
+            $dscProduto = $pp['DSC_PRODUTO'];
+            $codPedido = $pp['COD_PEDIDO'];
+
+            $SQL = "SELECT MSP.COD_MAPA_SEPARACAO_PRODUTO,
+                           (MSP.QTD_SEPARAR * MSP.QTD_EMBALAGEM) as QTD_SEPARAR,
+                           P.COD_PESSOA,
+                           MSP.COD_MAPA_SEPARACAO
+                      FROM MAPA_SEPARACAO_PRODUTO MSP
+                     INNER JOIN MAPA_SEPARACAO_QUEBRA MSQ ON MSQ.COD_MAPA_SEPARACAO = MSP.COD_MAPA_SEPARACAO AND MSQ.IND_TIPO_QUEBRA = 'T'
+                     INNER JOIN PEDIDO_PRODUTO PP ON PP.COD_PEDIDO_PRODUTO = MSP.COD_PEDIDO_PRODUTO
+                     INNER JOIN PEDIDO P ON P.COD_PEDIDO = PP.COD_PEDIDO
+                     WHERE MSP.COD_PEDIDO_PRODUTO = " . $codPP . " ORDER BY COD_MAPA_SEPARACAO";
+            $mspEmbalados = $this->getEntityManager()->getConnection()->query($SQL)->fetchAll(\PDO::FETCH_ASSOC);
+
+            $SQL = " SELECT MSP.COD_MAPA_SEPARACAO, MSP.COD_MAPA_SEPARACAO_PRODUTO, (MSP.QTD_SEPARAR * MSP.QTD_EMBALAGEM) as QTD_PRODUTO
+                       FROM MAPA_SEPARACAO_PRODUTO MSP
+                      INNER JOIN MAPA_SEPARACAO_QUEBRA MSQ ON MSQ.COD_MAPA_SEPARACAO = MSP.COD_MAPA_SEPARACAO
+                      INNER JOIN (SELECT *
+                                    FROM MAPA_SEPARACAO_PEDIDO MSP
+                                    LEFT JOIN PEDIDO_PRODUTO PP ON PP.COD_PEDIDO_PRODUTO = MSP.COD_PEDIDO_PRODUTO
+                                   WHERE MSP.COD_PEDIDO_PRODUTO = $codPP) MSPED
+                              ON MSPED.COD_MAPA_SEPARACAO = MSP.COD_MAPA_SEPARACAO
+                             AND MSPED.COD_PRODUTO = MSP.COD_PRODUTO
+                             AND MSPED.DSC_GRADE = MSP.DSC_GRADE
+                      WHERE MSQ.IND_TIPO_QUEBRA <> 'T'";
+            $mspNaoEmbalados = $this->getEntityManager()->getConnection()->query($SQL)->fetchAll(\PDO::FETCH_ASSOC);
+
+            if (count($mspEmbalados) > 0) {
+                $codPessoa = $mspEmbalados[0]['COD_PESSOA'];
+                $codMapa = $mspEmbalados[0]['COD_MAPA_SEPARACAO'];
+                $qtdConferida = 0;
+
+                $SQL = "SELECT COD_PRODUTO, DSC_GRADE, SUM(QTD_CONFERIDA * QTD_EMBALAGEM) as QTD_CONF, COD_PESSOA, COD_MAPA_SEPARACAO
+                         FROM MAPA_SEPARACAO_CONFERENCIA
+                        WHERE COD_PRODUTO = '$codProduto'
+                          AND DSC_GRADE = '$dscGrade'
+                          AND COD_PESSOA = $codPessoa
+                          AND COD_MAPA_SEPARACAO = $codMapa
+                        GROUP BY COD_PRODUTO, DSC_GRADE, COD_PESSOA, COD_MAPA_SEPARACAO";
+                $confEmbalados =  $this->getEntityManager()->getConnection()->query($SQL)->fetchAll(\PDO::FETCH_ASSOC);
+                if (count($confEmbalados)>0) {
+                    $qtdConferida = $confEmbalados[0]['QTD_CONF'];
+                }
+
+                foreach ($mspEmbalados as $mapa) {
+                    $mspId = $mapa['COD_MAPA_SEPARACAO_PRODUTO'];
+                    $qtdSepararMapa = $mapa['QTD_SEPARAR'];
+                    if ($qtdConferida >= $qtdSepararMapa) {
+                        $qtdConfMSP = $qtdSepararMapa;
+                    } else {
+                        $qtdConfMSP = $qtdConferida;
+                    }
+                    $qtdPendente = $qtdSepararMapa - $qtdConfMSP;
+                    if ($qtdPendente >0) {
+                        $mspEn = $this->find($mspId);
+                        if ($mspEn != null) {
+                            $mspEn->setQtdCortado($qtdPendente);
+                            $this->getEntityManager()->persist($mspEn);
+                            $qtdCortar = $qtdCortar - $qtdPendente;
+                        }
+                    }
+                    $qtdConferida = $qtdConferida - $qtdConfMSP;
+                }
+            }
+
+            if (count($mspNaoEmbalados) > 0) {
+                $codMapa = $mspNaoEmbalados[0]['COD_MAPA_SEPARACAO'];
+                $qtdConferida = 0;
+
+                $SQL = "SELECT COD_PRODUTO, DSC_GRADE, SUM(QTD_CONFERIDA * QTD_EMBALAGEM) as QTD_CONF, COD_MAPA_SEPARACAO
+                         FROM MAPA_SEPARACAO_CONFERENCIA
+                        WHERE COD_PRODUTO = '$codProduto'
+                          AND DSC_GRADE = '$dscGrade'
+                          AND COD_MAPA_SEPARACAO = $codMapa
+                        GROUP BY COD_PRODUTO, DSC_GRADE, COD_PESSOA, COD_MAPA_SEPARACAO";
+                $confNEmbalados =  $this->getEntityManager()->getConnection()->query($SQL)->fetchAll(\PDO::FETCH_ASSOC);
+                if (count($confNEmbalados)>0) {
+                    $qtdConferida = $confNEmbalados[0]['QTD_CONF'];
+                }
+
+                foreach ($mspNaoEmbalados as $mapa) {
+                    $mspId = $mapa['COD_MAPA_SEPARACAO_PRODUTO'];
+                    $qtdSepararMapa = $mapa['QTD_PRODUTO'];
+                    if ($qtdConferida >= $qtdSepararMapa) {
+                        $qtdConfMSP = $qtdSepararMapa;
+                    } else {
+                        $qtdConfMSP = $qtdConferida;
+                    }
+                    $qtdPendente = $qtdSepararMapa - $qtdConfMSP;
+                    if ($qtdPendente >0) {
+                        $mspEn = $this->find($mspId);
+                        if ($mspEn != null) {
+                            $mspEn->setQtdCortado($qtdPendente);
+                            $this->getEntityManager()->persist($mspEn);
+                            $qtdCortar = $qtdCortar - $qtdPendente;
+                        }
+                    }
+                    $qtdConferida = $qtdConferida - $qtdConfMSP;
+                }
+            }
+
+            if ($qtdCortar >0) {
+                throw new \Exception("Quantidade Cortada + Quantidade Conferida do Produto excede a quantidade solicitada no pedido $codPedido para o produto $codProduto/$dscGrade - $dscProduto");
+            }
+
+        }
+
+        $this->getEntityManager()->flush();
+        return true;
+    }
+
     public function validaCorteMapasERP($produtosERP) {
 
         $arrayPedidos = array();
@@ -36,7 +174,8 @@ class MapaSeparacaoProdutoRepository extends EntityRepository
                        MSP.DSC_GRADE,
                        SUM(MSP.QTD_SEPARAR) as QTD_TOTAL,
                        SUM(NVL(MSC.QTD_CONF,0)) as QTD_CONF,
-                       NVL(C.CORTE,0) as QTD_CORTE
+                       NVL(C.CORTE,0) as QTD_CORTE,
+                       PROD.DSC_PRODUTO
                   FROM MAPA_SEPARACAO MS
                   LEFT JOIN (SELECT MSP.COD_MAPA_SEPARACAO,
                                     MSP.COD_PRODUTO,
@@ -56,9 +195,10 @@ class MapaSeparacaoProdutoRepository extends EntityRepository
                                FROM PEDIDO_PRODUTO PP
                               WHERE PP.COD_PEDIDO IN ($pedidos)
                                GROUP BY PP.COD_PRODUTO, PP.DSC_GRADE) C ON C.COD_PRODUTO = MSP.COD_PRODUTO AND C.DSC_GRADE = MSP.DSC_GRADE
+                  LEFT JOIN PRODUTO PROD ON PROD.COD_PRODUTO = MSP.COD_PRODUTO AND PROD.DSC_GRADE = MSP.DSC_GRADE
                  WHERE MS.COD_MAPA_SEPARACAO IN ($mapas)
                    AND C.CORTE >0
-                 GROUP BY MSP.COD_PRODUTO, MSP.DSC_GRADE, C.CORTE";
+                 GROUP BY MSP.COD_PRODUTO, MSP.DSC_GRADE, C.CORTE, PROD.DSC_PRODUTO";
         $produtos =  $this->getEntityManager()->getConnection()->query($SQL)->fetchAll(\PDO::FETCH_ASSOC);
         $produtosCortar = array();
 
@@ -68,18 +208,17 @@ class MapaSeparacaoProdutoRepository extends EntityRepository
             $qtdMapa = $produto['QTD_TOTAL'];
             $qtdConferido = $produto['QTD_CONF'];
             $qtdCorte = $produto['QTD_CORTE'];
+            $dscProduto = $produto['DSC_PRODUTO'];
             if ($qtdCorte + $qtdConferido > $qtdMapa) {
-                throw new \Exception("Quantidade do conferida ($qtdConferido) + Quantidade Cortada no ERP ($qtdCorte), excede a quantidade solicitada na separação para o produto $codProduto - $grade");
+                throw new \Exception("Quantidade conferida ($qtdConferido) + Quantidade Cortada no ERP ($qtdCorte), excede a quantidade solicitada na separação para o produto $codProduto/$grade - $dscProduto");
             }
-            if ($qtdCorte + $qtdConferido > $qtdMapa) {
+            if ($qtdCorte + $qtdConferido == $qtdMapa) {
                 $produtosCortar[] = array('codProduto' =>$codProduto,
                                           'grade'=>$grade);
             }
         }
 
-
-
-        return $produtosCortar;
+        return $this->efetivaCorteMapasERP($pedidos,$produtosCortar);
     }
 
     public function getMapaProdutoByProdutoAndMapa($idMapa, $idProduto, $grade)
