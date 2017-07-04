532db5c9b5ae824ea876474e76c07db224448eba
LuisBastos <rodriguesbastos@gmail.com>
2017-07-04 17:23:28 -0300

    mudancas em telas #23

--- library/Wms/Module/Mobile/Form/ReabastecimentoManual.php
+++ library/Wms/Module/Mobile/Form/ReabastecimentoManual.php
@@ -12,7 +12,7 @@ class ReabastecimentoManual extends \Core\Form
                 'required' => true,
                 'label' => 'Etiqueta:',
                 'size' => 40,
-                'class' => 'focus',
+                'class' => 'focus form-control',
                 'maxlength' => 100,
                 'style' => 'width: 99%',
             ))
@@ -20,12 +20,13 @@ class ReabastecimentoManual extends \Core\Form
                 'required' => true,
                 'label' => 'Quantidade:',
                 'size' => 40,
+                'class' => 'form-control',
                 'maxlength' => 100,
                 'style' => 'width: 99%',
             ))
             ->addElement('submit', 'submit', array(
                 'label' => 'Buscar',
-                'class' => 'btn',
+                'class' => 'btn gradientBtn',
                 'decorators' => array('ViewHelper'),
             ))
             ->addElement('hidden', 'codOs')
