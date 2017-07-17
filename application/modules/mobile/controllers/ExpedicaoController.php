d2e25b0722592dd55da102ef4bd4a21bd4c271c4
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-07-17 12:26:14 -0300

    correcao de listagem de produtos pendentes de conferencia

--- application/modules/mobile/controllers/ExpedicaoController.php
+++ application/modules/mobile/controllers/ExpedicaoController.php
@@ -291,7 +291,7 @@ class Mobile_ExpedicaoController extends Action {
             }
         } catch (\Exception $e) {
             if ($confereQtd == true) {
-                $vetRetorno = array('retorno' => array('resposta' => 'error', 'message' => $e->getMessage()));
+                $vetRetorno = array('retorno' => array('resposta' => 'error', 'message' => $e->getMessage()), 'dados' => $produtosMapa);
                 $this->_helper->json($vetRetorno);
             } else {
                 $this->addFlashMessage('error',$e->getMessage());
