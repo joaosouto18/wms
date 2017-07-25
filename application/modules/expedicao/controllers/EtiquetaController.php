07ff815d0dab6dd6064943e107b99569a4eee9e3
tarcisiocms <tarcisiocms@outlook.com>
2017-07-25 08:45:44 -0300

    Remoção de max execution time geração de mapa

--- application/modules/expedicao/controllers/EtiquetaController.php
+++ application/modules/expedicao/controllers/EtiquetaController.php
@@ -51,7 +51,7 @@ class Expedicao_EtiquetaController  extends Action
             'produto'             => $em->getRepository('wms:Produto')
         );
 
-        ini_set('max_execution_time', 3000);
+        ini_set('max_execution_time', -1);
         ini_set('memory_limit', '-1');
         $idExpedicao    = $this->getRequest()->getParam('id');
         $central        = $this->getRequest()->getParam('central');
