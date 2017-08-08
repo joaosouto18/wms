72f920b2c7a28ee70f1441dfbd328882d7049e6b
LuisBastos <rodriguesbastos@gmail.com>
2017-08-08 13:30:42 -0300

    corte automatico

---
+++ docs/DB/v5.0.0/32-corte-automatico-andamento-exp.sql
@@ -0,0 +1,12 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  Luis Fernando
+ * Created: 08/08/2017
+ */
+
+ALTER TABLE PEDIDO_PRODUTO
+  ADD (QTD_CORTADO_AUTOMATICO NUMBER(13,3) NULL);
