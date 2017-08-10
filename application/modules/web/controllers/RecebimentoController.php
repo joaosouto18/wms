8a1cb07cc8844323c4415afadde8583efaef2fd3
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-08-10 17:32:50 -0300

    correcao de getcodrecebimentoERP

--- application/modules/web/controllers/RecebimentoController.php
+++ application/modules/web/controllers/RecebimentoController.php
@@ -663,9 +663,10 @@ class Web_RecebimentoController extends \Wms\Controller\Action {
                         $this->em->persist($ordemServicoEntity);
                         $this->em->flush();
 
+                        $notasFiscaisEntities = $this->getEntityManager()->getRepository('wms:Notafiscal')->findBy(array('recebimento' => $recebimentoEntity));
                         $recebimentoErp = false;
-                        foreach ($notasFiscais as $notaFiscalEntity) {
-                            if (!is_null($notaFiscalEntity->codRecebimentoErp())) {
+                        foreach ($notasFiscaisEntities as $notaFiscalEntity) {
+                            if (!is_null($notaFiscalEntity->getCodRecebimentoErp())) {
                                 $recebimentoErp = true;
                                 break;
                             }
