f2533712d18280122428c141964b619fbdbf591d
tarcisiocms <tarcisiocms@outlook.com>
2017-08-16 17:22:26 -0300

    Correção create data validade reserva

--- library/Wms/Domain/Entity/Ressuprimento/ReservaEstoqueRepository.php
+++ library/Wms/Domain/Entity/Ressuprimento/ReservaEstoqueRepository.php
@@ -336,7 +336,7 @@ class ReservaEstoqueRepository extends EntityRepository
                 $reservaEstoqueProduto->setProdutoVolume($this->getEntityManager()->getReference("wms:Produto\Volume",$produto['codProdutoVolume']));
                 $reservaEstoqueProduto->setCodProdutoVolume($produto['codProdutoVolume']);
             }
-            $dataValidade = isset($produto['validade']) && !empty($produto['validade']) ? new \DateTime($produto['validade']) : null;
+            $dataValidade = isset($produto['validade']) && !empty($produto['validade']) ? date_create_from_format('d/m/Y',$produto['validade']): null;
             $reservaEstoqueProduto->setQtd($produto['qtd']);
             $reservaEstoqueProduto->setReservaEstoque($reservaEstoque);
             $reservaEstoqueProduto->setValidade($dataValidade);
