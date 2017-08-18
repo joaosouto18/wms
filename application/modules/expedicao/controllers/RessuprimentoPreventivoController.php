f22d0a53056cedd4ffe00fead7537b51ff0ae587
LuisBastos <rodriguesbastos@gmail.com>
2017-08-18 14:24:57 -0300

    ressuprimento preventivo controller #32

--- application/modules/expedicao/controllers/RessuprimentoPreventivoController.php
+++ application/modules/expedicao/controllers/RessuprimentoPreventivoController.php
@@ -7,12 +7,47 @@
  */
 
 use Wms\Module\Web\Controller\Action,
-    Wms\Module\Web\Page;
+    Wms\Module\Web\Page,
+    Core\Util\Produto as ProdutoUtil;
 
 class Expedicao_RessuprimentoPreventivoController extends Action {
 
     public function indexAction() {
-//        var_dump('luis');die;
+        $this->configurePage();
+        $form = new \Wms\Module\Expedicao\Form\RessuprimentoPreventivo();
+        $form->init();
+        $data = $this->_getAllParams();
+        $form->populate($data);
+        $this->view->form = $form;
+    }
+
+    public function listAjaxAction() {
+        $data = $this->_getAllParams();
+        var_dump($data);die;
+    }
+
+    public function configurePage() {
+        $buttons[] = array(
+            'label' => 'Limpar Pulmão',
+            'cssClass' => 'button limpar-pulmao',
+            'urlParams' => array(
+                'module' => 'expedicao',
+                'controller' => 'ressuprimento-preventivo',
+                'action' => 'index',
+            ),
+            'tag' => 'a'
+        );
+        $buttons[] = array(
+            'label' => 'Completar Picking',
+            'cssClass' => 'button completar-picking',
+            'urlParams' => array(
+                'module' => 'expedicao',
+                'controller' => 'ressuprimento-preventivo',
+                'action' => 'index',
+            ),
+            'tag' => 'a'
+        );
+        Page::configure(array('buttons' => $buttons));
     }
 
 }
---
+++ library/Wms/Module/Expedicao/Form/RessuprimentoPreventivo.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Wms\Module\Expedicao\Form;
+
+use Wms\Module\Web\Form;
+use Wms\Domain\Entity\Deposito\Endereco;
+class RessuprimentoPreventivo extends Form {
+
+    public function init() {
+        $repoLinhaSeparacao = $this->getEm()->getRepository('wms:Armazenagem\LinhaSeparacao');
+        $repoLinhaSeparacao = $this->getEm()->getRepository('wms:Armazenagem\LinhaSeparacao');
+        $this->setAttribs(array(
+            'method' => 'post',
+            'class' => 'filtro',
+            'id' => 'cadastro-movimentacao',
+        ));
+        $this->addElement('select', 'volumes', array(
+                    'label' => 'Volumes',
+                ))
+                ->addElement('text', 'embalagens', array(
+                    'size' => 3,
+                    'label' => 'Embalagens',
+                    'class' => 'ctrSize',
+                ))
+                ->addElement('text', 'rua', array(
+                    'size' => 3,
+                    'label' => 'Rua Inicio',
+                    'alt' => 'enderecoRua',
+                    'class' => 'ctrSize',
+                ))
+                ->addElement('text', 'predio', array(
+                    'size' => 3,
+                    'alt' => 'enderecoPredio',
+                    'label' => 'Predio Inicio',
+                    'class' => 'ctrSize',
+                ))
+                ->addElement('text', 'nivel', array(
+                    'size' => 3,
+                    'alt' => 'enderecoNivel',
+                    'label' => 'Nivel Inicio',
+                    'class' => 'ctrSize',
+                ))
+                ->addElement('text', 'apto', array(
+                    'size' => 3,
+                    'alt' => 'enderecoApartamento',
+                    'label' => 'Apto Inicio',
+                    'class' => 'ctrSize',
+                ))
+                ->addElement('select', 'ladoRua', array(
+                    'label' => 'Lado da Rua',
+                    'mostrarSelecione' => true,
+                    'multiOptions' => array('firstOpt' => 'Todos', 'options' => array(1 => 'Par', 2 => 'Ímpar')),
+                ))
+                ->addElement('select', 'tipoEndereco', array(
+                    'label' => 'Tipo de End. Picking',
+                    'multiOptions' => array('firstOpt' => 'Ambos', 'options' => array(
+                            Endereco::ENDERECO_PICKING => Endereco::$tiposEndereco[Endereco::ENDERECO_PICKING],
+                            Endereco::ENDERECO_PULMAO => Endereco::$tiposEndereco[Endereco::ENDERECO_PULMAO],
+                            Endereco::ENDERECO_PICKING_DINAMICO => Endereco::$tiposEndereco[Endereco::ENDERECO_PICKING_DINAMICO],
+                        ))
+                ))
+                ->addElement('select', 'linhaSeparacao', array(
+                    'label' => 'Linha de Separação',
+                    'mostrarSelecione' => true,
+                    'multiOptions' => array('firstOpt' => 'Todas', 'options' => $repoLinhaSeparacao->getIdValue()),
+                ))
+                ->addElement('select', 'tiporessuprimento', array(
+                    'label' => 'Tipo Ressuprimento',
+                    'mostrarSelecione' => true,
+                    'multiOptions' => array('firstOpt' => 'Apenas Pulmão Completo', 'options' => array(1 => 'Completar Picking')),
+                ))
+                ->addElement('text', 'ocupacao', array(
+                    'size' => 3,
+                    'label' => 'Ocupação %',
+                    'alt' => 'enderecoPredio',
+                    'class' => 'ctrSize',
+                ))
+                ->addElement('text', 'ruaFinal', array(
+                    'size' => 3,
+                    'label' => 'Rua Final',
+                    'alt' => 'enderecoRua',
+                    'class' => 'ctrSize',
+                ))
+                ->addElement('text', 'predioFinal', array(
+                    'size' => 3,
+                    'alt' => 'enderecoPredio',
+                    'label' => 'Predio Final',
+                    'class' => 'ctrSize',
+                ))
+                ->addElement('text', 'nivelFinal', array(
+                    'size' => 3,
+                    'alt' => 'enderecoNivel',
+                    'label' => 'Nivel Final',
+                    'class' => 'ctrSize',
+                ))
+                ->addElement('text', 'aptoFinal', array(
+                    'size' => 3,
+                    'alt' => 'enderecoApartamento',
+                    'label' => 'Apto Final',
+                    'class' => 'ctrSize',
+                ))
+                ->addElement('submit', 'buscar', array(
+                    'label' => 'Buscar',
+                    'class' => 'btn',
+                    'decorators' => array('ViewHelper')
+                ))
+                ->addDisplayGroup(array('rua', 'predio', 'nivel', 'apto', 'ladoRua', 'tipoEndereco', 'linhaSeparacao', 'ruaFinal', 'predioFinal', 'nivelFinal', 'aptoFinal'), 'identificacao', array('legend' => 'Filtros'))
+                ->addDisplayGroup(array('ocupacao', 'tiporessuprimento', 'buscar'), 'tranferencia', array('legend' => 'Parâmetros'));
+    }
+
+}
