5277076115ba54e660729d0e15cf81d3ae350150
LuisBastos <rodriguesbastos@gmail.com>
2017-08-18 11:42:05 -0300

    ressuprimento preventivo controller

---
+++ application/modules/expedicao/controllers/RessuprimentoPreventivoController.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+use Wms\Module\Web\Controller\Action,
+    Wms\Module\Web\Page;
+
+class Expedicao_RessuprimentoPreventivoController extends Action {
+
+    public function indexAction() {
+//        var_dump('luis');die;
+    }
+
+}
