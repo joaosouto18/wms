b27ad477556b42d1516552a09c3ea31ce6c8d27b
Lucas <lucaschinelate@hotmail.com>
2017-08-29 00:29:25 -0300

    teste

--- library/Wms/Domain/Entity/Expedicao/EtiquetaSeparacaoRepository.php
+++ library/Wms/Domain/Entity/Expedicao/EtiquetaSeparacaoRepository.php
@@ -1837,7 +1837,8 @@ class EtiquetaSeparacaoRepository extends EntityRepository
             $mapaProduto->setQtdCortado(0);
             $mapaProduto->setIndConferido('N');
             $mapaProduto->setDepositoEndereco($depositoEndereco);
-            $mapaProduto->setCubagem(number_format($cubagem,4,"."));
+            //$mapaProduto->setCubagem(number_format($cubagem,4,"."));
+            $mapaProduto->setCubagem($cubagem);
         } else {
             $mapaProduto->setQtdSeparar($mapaProduto->getQtdSeparar() + $quantidadePedido);
         }
