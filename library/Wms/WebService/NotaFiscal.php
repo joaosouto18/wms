a0329344e3b5245e42e8c0698a97d1652a071b19
Lucas Chinelate <lucaschinelate@hotmail.com>
2017-09-13 15:52:16 -0300

    Teste integração

--- library/Wms/WebService/NotaFiscal.php
+++ library/Wms/WebService/NotaFiscal.php
@@ -366,7 +366,6 @@ class Wms_WebService_NotaFiscal extends Wms_WebService
      * @param string $itens Itens da Nota {Json}
      * @param string $bonificacao Indica se a nota fiscal é ou não do tipo bonificação, Por padrão Não (N).
      * @param string $observacao Observações da Nota Fiscal
-     * * @param string $pesoTotal Peso Total da Nota Fiscal
      * @return boolean
      */
     public function salvarJson($idFornecedor, $numero, $serie, $dataEmissao, $placa, $itens, $bonificacao, $observacao){
