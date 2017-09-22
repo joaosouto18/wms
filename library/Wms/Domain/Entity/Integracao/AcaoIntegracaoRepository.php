ea3414e4edf1083cf1ec9d31aeb5a118c162c1f2
LuisBastos <rodriguesbastos@gmail.com>
2017-09-22 09:56:57 -0300

    inventario

--- library/Wms/Domain/Entity/Integracao/AcaoIntegracaoRepository.php
+++ library/Wms/Domain/Entity/Integracao/AcaoIntegracaoRepository.php
@@ -179,6 +179,7 @@ class AcaoIntegracaoRepository extends EntityRepository
 
         $this->_em->clear();
         $acaoEn = $this->findOneBy(array('id'=>$idAcao));
+
         $sucess = "S";
         $observacao = "";
         $trace = "";
@@ -220,7 +221,6 @@ class AcaoIntegracaoRepository extends EntityRepository
 
             //STRING DA QUERY DE INTEGRAÇÃO
             $query = $acaoFiltroRepo->getQuery($acaoEn, $options, $filtro, $data);
-        echo $query;die;
 
             if ($dados == null) {
                 $words = explode(" ",trim($query));
