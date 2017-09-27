b955022b2ac881f043a09530dacdc940e7bc4435
tarcisiocms <tarcisiocms@outlook.com>
2017-09-27 14:18:31 -0300

    Retorno de status do recebimento quando endereçado

--- docs/DB/v6.0.0/5-cadastro-peso-embalagem.sql
+++ docs/DB/v6.0.0/5-cadastro-peso-embalagem.sql
@@ -7,7 +7,7 @@
  * Author:  Luis Fernando
  * Created: 19/09/2017
  */
-INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, '6.0.0','xx-cadastro-peso-embalagem.sql');
+INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, '6.0.0','5-cadastro-peso-embalagem.sql');
 
 ALTER TABLE PRODUTO_EMBALAGEM 
   ADD NUM_ALTURA NUMBER(15,3) DEFAULT 0 NULL;
