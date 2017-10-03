c96db5f7b150104211d95c8697b2c421d0fac0ed
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-10-03 17:17:30 -0300

    correcao de variavel vindo de parametro de webservice de expedicao

--- library/Wms/WebService/Expedicao.php
+++ library/Wms/WebService/Expedicao.php
@@ -763,13 +763,13 @@ class Wms_WebService_Expedicao extends Wms_WebService
             $idProduto = ProdutoUtil::formatar(trim($produto['codProduto']));
 
             $enProduto = $ProdutoRepo->find(array('id' => $idProduto, 'grade' => $produto['grade']));
-            if (isset($produto['quantidade'])) {
-                $produto['qtde'] = $produto['quantidade'];
+            if (isset($produto['qtde'])) {
+                $produto['quantidade'] = $produto['qtde'];
             }
             $qtdCorrigida = str_replace(',','.',$produto['qtde']);
 
             if(isset($prod[$idProduto.'--'.$produto['grade']])){
-                $prod[$idProduto.'--'.$produto['grade']]['qtde'] = \Wms\Math::adicionar($prod[$idProduto.'--'.$produto['grade']]['qtde'], $qtdCorrigida);
+                $prod[$idProduto.'--'.$produto['grade']]['quantidade'] = \Wms\Math::adicionar($prod[$idProduto.'--'.$produto['grade']]['quantidade'], $qtdCorrigida);
             }else{
                 $prod[$idProduto.'--'.$produto['grade']] = array(
                     'codPedido' => $enPedido->getId(),
