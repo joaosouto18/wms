e63cada14eb14d111fb5ff2cf200c93a0192bc72
Lucas Chinelate <lucaschinelate@hotmail.com>
2017-10-17 14:51:22 -0200

    Teste

--- library/Wms/WebService/NotaFiscal.php
+++ library/Wms/WebService/NotaFiscal.php
@@ -237,15 +237,15 @@ class Wms_WebService_NotaFiscal extends Wms_WebService
      * @param string $serie Serie da nota fiscal
      * @param string $dataEmissao Data de emissao da nota fiscal. Formato esperado (d/m/Y) ex:'22/11/2010'
      * @param string $placa Placa do veiculo vinculado à nota fiscal formato esperado: XXX0000
-     * @param string $cnpjDestinatario CNPJ da filial dona da nota
      * @param array $itens
      * @param string $bonificacao Indica se a nota fiscal é ou não do tipo bonificação, Por padrão Não (N).
-     * @param string $tipoNota Identifica se é uma nota de Bonificação(B), Compra(C), etc.
      * @param string $observacao Observações da Nota Fiscal
+     * @param string $tipoNota Identifica se é uma nota de Bonificação(B), Compra(C), etc.
+     * @param string $cnpjDestinatario CNPJ da filial dona da nota
      * @return boolean
      * @throws Exception
      */
-    public function salvar($idFornecedor, $numero, $serie, $cnpjDestinatario, $dataEmissao, $placa, $itens, $bonificacao, $tipoNota, $observacao)
+    public function salvar($idFornecedor, $numero, $serie, $dataEmissao, $placa, $itens, $bonificacao, $observacao, $tipoNota, $cnpjDestinatario)
     {
         $em = $this->__getDoctrineContainer()->getEntityManager();
         try{
