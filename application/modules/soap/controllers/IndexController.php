cf9fa118ae92a868aed42ec2f39a2d66f86ff6fd
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-10-17 14:46:07 -0300

    debugando o WS sabium

--- application/modules/soap/controllers/IndexController.php
+++ application/modules/soap/controllers/IndexController.php
@@ -81,7 +81,6 @@ class Soap_IndexController extends Core\Controller\Action\WebService
      */
     public function indexAction()
     {
-        var_dump('abc');
         $serviceName = ucwords($this->serviceName);
         // initialize server and set WSDL file location
         $server = new \Zend_Soap_Server($this->conf->soap->{$this->serviceName}->wsdl);
@@ -92,7 +91,6 @@ class Soap_IndexController extends Core\Controller\Action\WebService
         // register exceptions that generate SOAP faults
         $server->registerFaultException('Exception');
         // handle request
-        var_dump($server); exit;
         $server->handle();
     }
 
