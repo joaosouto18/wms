57e2eb33facbd16ddadd07527b7888b50033ff8c
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-10-20 15:51:20 -0300

    cadastro de volume

--- library/Wms/Domain/Entity/Expedicao/EtiquetaSeparacaoRepository.php
+++ library/Wms/Domain/Entity/Expedicao/EtiquetaSeparacaoRepository.php
@@ -116,7 +116,6 @@ class EtiquetaSeparacaoRepository extends EntityRepository
                 ->setParameter('idCarga', $idCarga);
         }
 
-        echo $dql->getQuery()->getSql(); exit;
         return $dql->getQuery()->getSingleScalarResult();
     }
 
