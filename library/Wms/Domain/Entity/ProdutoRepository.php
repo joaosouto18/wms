ab98d33739854877526a91bc3356e1405434eb03
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-10-20 14:16:24 -0300

    cadastro de volume

--- library/Wms/Domain/Entity/ProdutoRepository.php
+++ library/Wms/Domain/Entity/ProdutoRepository.php
@@ -581,7 +581,6 @@ class ProdutoRepository extends EntityRepository implements ObjectRepository {
 
         $volumeRepo = $em->getRepository('wms:Produto\Volume');
 
-        var_dump($volumes); exit;
         foreach ($volumes as $id => $itemVolume) {
             extract($itemVolume);
 
