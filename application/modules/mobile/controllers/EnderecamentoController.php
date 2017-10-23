2efaa063d4d8f612cd9083a14e47391a1adaf176
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-10-23 11:11:20 -0200

    transferencia de estoque no coletor para volume

--- application/modules/mobile/controllers/EnderecamentoController.php
+++ application/modules/mobile/controllers/EnderecamentoController.php
@@ -871,6 +871,7 @@ class Mobile_EnderecamentoController extends Action
             if (!empty($codBarrasUma)) {
                 /** @var \Wms\Domain\Entity\Enderecamento\PaleteProdutoRepository $paleteProdutoRepo */
                 $paleteProdutoRepo = $this->em->getRepository('wms:Enderecamento\PaleteProduto');
+                $produtoVolumeRepo = $this->getEntityManager()->getRepository('wms:Produto\Volume');
 
                 /** @var \Wms\Domain\Entity\Enderecamento\PaleteProduto $paleteProduto */
                 $paleteProduto = $paleteProdutoRepo->findOneBy(array('uma' => $codBarrasUma));
@@ -879,8 +880,10 @@ class Mobile_EnderecamentoController extends Action
 
                 $codProduto = $paleteProduto->getCodProduto();
                 $grade = $paleteProduto->getGrade();
+                $produtoVolumeEntity = $produtoVolumeRepo->findOneBy(array('codProduto' => $codProduto, 'grade' => $grade));
 
-                $embalagemEn = $paleteProduto->getEmbalagemEn();
+                if (!isset($produtoVolumeEntity) || empty($produtoVolumeEntity))
+                    $embalagemEn = $paleteProduto->getEmbalagemEn();
 
             } else if (!empty($codBarras)) {
                 $LeituraColetor = new \Wms\Service\Coletor();
@@ -904,7 +907,7 @@ class Mobile_EnderecamentoController extends Action
             if (empty($estoqueEn))
                 throw new Exception("Não foi encontrado o estoque com endereco " . $endereco->getDescricao() . " produto " . $codProduto . " grade " . $grade);
 
-            if ($embalagemEn != null && !empty($embalagemEn)) {
+            if ($embalagemEn != null) {
                 $qtd = floor($estoqueEn->getQtd() / $embalagemEn->getQuantidade());
                 $qtdReal = floor($estoqueEn->getQtd() / $embalagemEn->getQuantidade()) * $embalagemEn->getQuantidade();
                 $qtdEmbalagem = $embalagemEn->getQuantidade();
