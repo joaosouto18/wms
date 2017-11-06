cdbff071baa69bce1d975285f65277aa04e5ae18
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-11-06 17:27:54 -0200

    teste integracao NF

--- library/Wms/Service/Integracao.php
+++ library/Wms/Service/Integracao.php
@@ -858,6 +858,11 @@ class Integracao {
             $x = $x + 1;
             switch ($this->getAcao()->getTipoAcao()->getId()) {
                 case AcaoIntegracao::INTEGRACAO_NOTAS_FISCAIS:
+                    var_dump($row['DAT_EMISSAO']);
+                    var_dump(\DateTime::createFromFormat('d/m/Y', $row['DAT_EMISSAO']));
+                    var_dump($row['DTH']);
+                    var_dump(\DateTime::createFromFormat('d/m/Y H:i:s', $row['DTH']));
+                    exit;
                     $nf = new TabelaTemporaria\NotaFiscalEntrada();
                     $nf->setCodFornecedor($row['COD_FORNECEDOR']);
                     $nf->setNomFornecedor($row['NOM_FORNECEDOR']);
@@ -867,11 +872,11 @@ class Integracao {
                     $nf->setNumNF($row['NUM_NOTA_FISCAL']);
                     $nf->setCodProduto($row['COD_PRODUTO']);
                     $nf->setSerieNF($row['COD_SERIE_NOTA_FISCAL']);
-                    $nf->setDthEmissao(\DateTime::createFromFormat('Y-m-d', $row['DAT_EMISSAO']));
+                    $nf->setDthEmissao(\DateTime::createFromFormat('d/m/Y', $row['DAT_EMISSAO']));
                     $nf->setVeiculo($row['DSC_PLACA_VEICULO']);
                     $nf->setQtdItem(str_replace(",", ".", $row['QTD_ITEM']));
                     $nf->setVlrTotal(str_replace(",", ".", $row['VALOR_TOTAL']));
-                    $nf->setDth(\DateTime::createFromFormat('Y-m-d H:i:s', $row['DTH']));
+                    $nf->setDth(\DateTime::createFromFormat('d/m/Y H:i:s', $row['DTH']));
                     $this->_em->persist($nf);
                     break;
                 case AcaoIntegracao::INTEGRACAO_PEDIDOS:
