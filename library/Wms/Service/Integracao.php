be3e5bfd4dc9a78f814e45f6f0e976f401c4ff95
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-11-06 18:09:22 -0200

    teste integracao NF

--- library/Wms/Service/Integracao.php
+++ library/Wms/Service/Integracao.php
@@ -867,11 +867,11 @@ class Integracao {
                     $nf->setNumNF($row['NUM_NOTA_FISCAL']);
                     $nf->setCodProduto($row['COD_PRODUTO']);
                     $nf->setSerieNF($row['COD_SERIE_NOTA_FISCAL']);
-                    $nf->setDthEmissao(\DateTime::createFromFormat('d/m/Y', $row['DAT_EMISSAO']));
+                    $nf->setDthEmissao(new \DateTime());
                     $nf->setVeiculo($row['DSC_PLACA_VEICULO']);
                     $nf->setQtdItem(str_replace(",", ".", $row['QTD_ITEM']));
                     $nf->setVlrTotal(str_replace(",", ".", $row['VALOR_TOTAL']));
-                    $nf->setDth(\DateTime::createFromFormat('d/m/Y H:i:s', $row['DTH']));
+                    $nf->setDth(new \DateTime());
                     $this->_em->persist($nf);
                     break;
                 case AcaoIntegracao::INTEGRACAO_PEDIDOS:
