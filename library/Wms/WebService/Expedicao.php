b10c82135fecfce3621479087dee96bd41284e60
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-11-09 10:10:44 -0200

    ajuste de mensagem de etiquetas cortadas

--- library/Wms/WebService/Expedicao.php
+++ library/Wms/WebService/Expedicao.php
@@ -877,7 +877,7 @@ class Wms_WebService_Expedicao extends Wms_WebService
                 $SQL = "SELECT *
                           FROM PEDIDO_PRODUTO PP
                          WHERE PP.COD_PEDIDO = '" . $pedido['codPedido'] . "'
-                           AND PP.QUANTIDADE <= NVL(PP.QTD_CORTADA,0) ";
+                           AND PP.QUANTIDADE > NVL(PP.QTD_CORTADA,0) ";
                 $countProdutosPendentesCorte = count($this->_em->getConnection()->query($SQL)->fetchAll(\PDO::FETCH_ASSOC));
 
                 if (($statusExpedicao->getId() == Expedicao::STATUS_INTEGRADO) ||
