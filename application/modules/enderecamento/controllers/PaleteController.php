f8fe5a199eac9f89aabf69589888c90fe29233db
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-11-14 11:41:01 -0200

    correção de barras na url de enderecamento

--- application/modules/enderecamento/controllers/PaleteController.php
+++ application/modules/enderecamento/controllers/PaleteController.php
@@ -15,6 +15,7 @@ class Enderecamento_PaleteController extends Action
         $codProduto     = $this->getRequest()->getParam('codigo');
         $grade          = $this->getRequest()->getParam('grade');
         $produtos       = $this->getRequest()->getParam('produtos');
+        $grade          = str_replace('&','/',$grade);
 
         /** @var \Wms\Domain\Entity\Enderecamento\PaleteRepository $paleteRepo */
         $paleteRepo = $this->em->getRepository('wms:Enderecamento\Palete');
