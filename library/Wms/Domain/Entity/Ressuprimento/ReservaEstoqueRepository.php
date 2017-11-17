c12b7ac29cce1f35d8322cb831a371d34fff6acd
Rodrigo Dantley <rodrigodantley@gmail.com>
2017-11-16 22:03:40 -0200

    correcao de corte de etiquetas

--- library/Wms/Domain/Entity/Ressuprimento/ReservaEstoqueRepository.php
+++ library/Wms/Domain/Entity/Ressuprimento/ReservaEstoqueRepository.php
@@ -78,7 +78,7 @@ class ReservaEstoqueRepository extends EntityRepository
             } else {
                 $reservaEstoqueExpedicaoRepo = $repositorios['reservaEstoqueExpRepo'];
             }
-            $reservaEstoqueArray = $reservaEstoqueExpedicaoRepo->findBy($idOrigem);
+            $reservaEstoqueArray = $reservaEstoqueExpedicaoRepo->findBy(array('expedicao'=> $idOrigem));
         }
 
 
