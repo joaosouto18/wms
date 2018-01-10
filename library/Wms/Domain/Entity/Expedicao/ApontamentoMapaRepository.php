38eadf61ed2c3a87e3c05f9555af80c2cea39938
LuisBastos <rodriguesbastos@gmail.com>
2018-01-10 18:09:34 -0200

    produtividade

--- library/Wms/Domain/Entity/Expedicao/ApontamentoMapaRepository.php
+++ library/Wms/Domain/Entity/Expedicao/ApontamentoMapaRepository.php
@@ -300,6 +300,7 @@ class ApontamentoMapaRepository extends EntityRepository {
                     MP.COD_EXPEDICAO,
                     PD.COD_PESSOA
                     ORDER BY $order";
+        echo $sql;
         $result = $this->getEntityManager()->getConnection()->query($sql)->fetchAll(\PDO::FETCH_ASSOC);
         $qtdRows = count($result);
         $pesoTotal = 0;
@@ -332,7 +333,6 @@ class ApontamentoMapaRepository extends EntityRepository {
             $result[$key]['QTD_CUBAGEM'] = number_format($value['QTD_CUBAGEM'], 2, ',', '.');
             $intervalo = date_diff($tempoInicial, $tempoFinal);
             $result[$key]['TEMPO_GASTO'] = $intervalo->format('%H:%I:%S');
-            $pesoTotal = $value['QTD_PESO'];
             $volumeTotal = $volumeTotal + $value['QTD_VOLUMES'];
             $pesoTotal = $pesoTotal + $value['QTD_PESO'];
             $cubagemTotal = $cubagemTotal + $value['QTD_CUBAGEM'];
@@ -344,7 +344,6 @@ class ApontamentoMapaRepository extends EntityRepository {
             $seconds += $i * 60;
             $seconds += $s % 60;
         }
-
         $hoje = date('Y-m-d H:i:s');
         $intervalo = date_diff(\DateTime::createFromFormat('Y-m-d H:i:s', $hoje), \DateTime::createFromFormat('Y-m-d H:i:s', date('Y-m-d H:i:s', strtotime($hoje . '+ ' . $seconds . ' seconds'))));
         $min = $intervalo->i;
@@ -361,9 +360,10 @@ class ApontamentoMapaRepository extends EntityRepository {
         } else {
             $tempoTotal = date('H:i:s', strtotime("$intervalo->h:$min:$sec"));
         }
+
         $result[$qtdRows]['NOM_PESSOA'] = 'TOTAIS';
         $result[$qtdRows]['IDENTIDADE'] = '-';
-        $result[$qtdRows]['QTD_PESO'] = number_format($pesoTotal, 2, ',', '.');;
+        $result[$qtdRows]['QTD_PESO'] = $pesoTotal;//number_format($pesoTotal, 2, ',', '.');
         $result[$qtdRows]['QTD_VOLUMES'] = number_format($volumeTotal, 2, ',', '.');
         $result[$qtdRows]['QTD_PRODUTOS'] = number_format($quantidadeTotal, 0, ',', '.');
         $result[$qtdRows]['QTD_CUBAGEM'] = number_format($cubagemTotal, 2, ',', '.');
