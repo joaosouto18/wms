deea17de34a2a7592bb9ee96e5d929b705ee2431
Rodrigo Dantley <rodrigodantley@gmail.com>
2018-02-08 16:41:46 -0200

    relatorio de produtos sem picking exportando em csv

--- library/Wms/Module/Expedicao/Printer/EtiquetaSeparacao.php
+++ library/Wms/Module/Expedicao/Printer/EtiquetaSeparacao.php
@@ -928,10 +928,9 @@ class EtiquetaSeparacao extends Pdf
         $this->SetFont('Arial', 'B', 9);
         $impressao  = utf8_decode("EXP:$etiqueta[codExpedicao] - PLACA:$etiqueta[placaExpedicao] - $etiqueta[tipoCarga]:$etiqueta[codCargaExterno]\n");
         $this->MultiCell(100, 4.5, $impressao, 0, 'L');
+        $this->SetFont('Arial', 'B', 8);
         if (strlen("$etiqueta[cliente]") <= 30) {
             $this->SetFont('Arial', 'B', 10);
-        } else {
-            $this->SetFont('Arial', 'B', 8);
         }
         $impressao = substr(utf8_decode("$etiqueta[cliente]"),0,40)."\n";
 
