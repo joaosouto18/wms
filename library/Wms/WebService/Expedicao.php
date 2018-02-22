769b6430bb0ec2e770b5765187ae26e1365fcfc1
LuisBastos <rodriguesbastos@gmail.com>
2018-02-22 09:33:52 -0300

    agrupar cargas

--- library/Wms/WebService/Expedicao.php
+++ library/Wms/WebService/Expedicao.php
@@ -1061,13 +1061,19 @@ class Wms_WebService_Expedicao extends Wms_WebService
 
     protected function findExpedicaoByPlacaExpedicao($repositorios, $placaExpedicao) {
         $ExpedicaoRepo      = $repositorios['expedicaoRepo'];
-        $entityExpedicao    = $ExpedicaoRepo->findOneBy(array('placaExpedicao' => $placaExpedicao, 'status' => array(Expedicao::STATUS_INTEGRADO, Expedicao::STATUS_EM_SEPARACAO, Expedicao::STATUS_EM_CONFERENCIA)));
-        if ($entityExpedicao == null) {
-            $entityExpedicao= $ExpedicaoRepo->save($placaExpedicao, false);
-        }
+        $parametroRepo = $this->_em->getRepository('wms:Sistema\Parametro');
+        $parametro = $parametroRepo->findOneBy(array('constante' => 'AGRUPAR_CARGAS'));
+        if($parametro->getValor() == 'N'){
+            $entityExpedicao = $ExpedicaoRepo->save($placaExpedicao, false);
+        }else {
+            $entityExpedicao = $ExpedicaoRepo->findOneBy(array('placaExpedicao' => $placaExpedicao, 'status' => array(Expedicao::STATUS_INTEGRADO, Expedicao::STATUS_EM_SEPARACAO, Expedicao::STATUS_EM_CONFERENCIA)));
+            if ($entityExpedicao == null) {
+                $entityExpedicao = $ExpedicaoRepo->save($placaExpedicao, false);
+            }
 
-        if ($entityExpedicao->getStatus()->getId() == \Wms\Domain\Entity\Expedicao::STATUS_FINALIZADO) {
-            throw new \Exception('Expedicao ' . $entityExpedicao->getId() . ' já está finalizada');
+            if ($entityExpedicao->getStatus()->getId() == \Wms\Domain\Entity\Expedicao::STATUS_FINALIZADO) {
+                throw new \Exception('Expedicao ' . $entityExpedicao->getId() . ' já está finalizada');
+            }
         }
 
         return $entityExpedicao;
