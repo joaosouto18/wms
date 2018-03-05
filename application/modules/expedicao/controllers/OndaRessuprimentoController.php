5c59199cc83f620a38778785324c3fd24838c034
Rodrigo Dantley <rodrigodantley@gmail.com>
2018-03-05 10:21:36 -0300

    correcoes relatorio

--- application/modules/expedicao/controllers/OndaRessuprimentoController.php
+++ application/modules/expedicao/controllers/OndaRessuprimentoController.php
@@ -168,7 +168,7 @@ class Expedicao_OndaRessuprimentoController extends Action {
             }
             /** @var \Wms\Domain\Entity\Ressuprimento\OndaRessuprimentoRepository $ondaRessuprimentoRepo */
             $ondaRessuprimentoRepo = $this->em->getRepository("wms:Ressuprimento\OndaRessuprimento");
-            $result = $ondaRessuprimentoRepo->getOndasEmAbertoCompleto($dataInicial, $dataFinal, $status, true, $idProduto, $idExpedicao, $operador);
+            $result = $ondaRessuprimentoRepo->getOndasEmAbertoCompleto($dataInicial, $dataFinal, $status, true, $idProduto, $idExpedicao, $operador, true);
             $Grid = new OsGrid();
             $Grid->init($result, $values)->render();
 
