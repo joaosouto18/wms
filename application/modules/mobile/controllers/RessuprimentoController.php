79ab13d8edf57871263967c36311ea33cc5c988f
Rodrigo Dantley <rodrigodantley@gmail.com>
2018-04-17 14:35:13 -0300

    correcao de formatacao de quantidade em ressuprimento preventivo no coletor

--- application/modules/mobile/controllers/RessuprimentoController.php
+++ application/modules/mobile/controllers/RessuprimentoController.php
@@ -150,7 +150,7 @@ class Mobile_RessuprimentoController extends Action
                     $this->view->grade = $grade = $result[0]['GRADE'];
                     $this->view->descricao = $result[0]['DESCRICAO'];
                     $this->view->endereco = $dscEndereco = $result[0]['ENDERECO'];
-                    $this->view->qtd = number_format($result[0]['QTD'], 2, ',', '.').' '.$result[0]['DSC_EMBALAGEM'];
+                    $this->view->qtd = $result[0]['QTD'];
                 }
 
             }
