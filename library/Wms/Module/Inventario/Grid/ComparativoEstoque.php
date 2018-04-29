f0b3ae7d4b748c5250e53b66354e09c0cc284704
Lucas Chinelate <lucaschinelate@hotmail.com>
2018-04-29 10:32:26 -0300

    Totais

--- library/Wms/Module/Inventario/Grid/ComparativoEstoque.php
+++ library/Wms/Module/Inventario/Grid/ComparativoEstoque.php
@@ -7,10 +7,10 @@ use Wms\Module\Web\Grid;
 
 class ComparativoEstoque extends Grid {
 
-    public function init($restult) {
+    public function init($result) {
 
         $this->setAttrib('title', 'comparativo-estoque');
-        $this->setSource(new \Core\Grid\Source\ArraySource($restult));
+        $this->setSource(new \Core\Grid\Source\ArraySource($result));
         $this->addColumn(array(
             'label' => 'Cod. Produto',
             'index' => 'COD_PRODUTO',
@@ -56,8 +56,108 @@ class ComparativoEstoque extends Grid {
 
         $this->setShowExport(false)
                 ->addMassAction('mass-select', 'Selecionar');
-        $pg = new Pager(count($restult), 0, count($restult));
+        $pg = new Pager(count($result), 0, count($result));
         $this->setPager($pg);
+
+        $qtdProdFalta = 0;
+        $qtdTotalFalta = 0;
+        $vlrTotalFalta = 0;
+        $qtdProdSobra = 0;
+        $qtdTotalSobra = 0;
+        $vlrTotalSobra = 0;
+        $qtdProdDivTotal = 0;
+        $qtdDivTotal = 0;
+        $vlrTotal = 0;
+
+        foreach ($result as $row) {
+            if ($row['DIVERGENCIA'] >0) { //SOBRA
+                $qtdProdSobra += 1;
+                $qtdTotalSobra += $row['DIVERGENCIA'];
+                $vlrTotalSobra += $row['VLR_DIVERGENCIA'];
+            } else { //FALTA
+                $qtdProdFalta += 1;
+                $qtdTotalFalta += $row['DIVERGENCIA'];
+                $vlrTotalFalta += $row['VLR_DIVERGENCIA'];
+            }
+        }
+
+        $qtdTotalFalta = $qtdTotalFalta * -1;
+        $vlrTotalFalta = $vlrTotalFalta * -1;
+
+        $qtdProdDivTotal = $qtdProdSobra + $qtdProdSobra;
+        $qtdDivTotal = $qtdTotalSobra + $qtdTotalFalta;
+        $vlrTotal = $vlrTotalSobra + $vlrTotalFalta;
+
+        $qtdProdFalta = number_format($qtdProdFalta,0);
+        $qtdTotalFalta = number_format($qtdTotalFalta,3);
+        $vlrTotalFalta = number_format($vlrTotalFalta,2);
+        $qtdProdSobra = number_format($qtdProdSobra);
+        $qtdTotalSobra = number_format($qtdTotalSobra,3);
+        $vlrTotalSobra = number_format($vlrTotalSobra,2);
+        $qtdProdDivTotal = number_format($qtdProdDivTotal,0);
+        $qtdDivTotal = number_format($qtdDivTotal,3);
+        $vlrTotal = number_format($vlrTotal,2);
+
+        echo '</br> </br>
+            <fieldset>
+                <legend>Resumo</legend>
+                <table width="100%">
+                    <tr>
+                        <td>
+                            <fieldset>
+                            <legend>Sobras</legend>
+                            <table width="100%">
+                                <tr>
+                                    <td>Qtd.Prod.</td>
+                                    <td>Qtd.Total</td>
+                                    <td>Vlr.Total</td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" style="text-align:right;" size="10" value="'. $qtdProdSobra . '" disabled=""/></td>
+                                    <td><input type="text" style="text-align:right;" size="15" value="'. $qtdTotalSobra . '" disabled=""/></td>
+                                    <td><input type="text" style="text-align:right;" size="15" value="'. $vlrTotalSobra . '" disabled=""/></td>
+                                </tr>
+                            </table>                
+                            </fieldset>                        
+                        </td>
+                        <td>
+                            <fieldset>
+                            <legend>Faltas</legend>
+                            <table width="100%">
+                                <tr>
+                                    <td>Qtd.Prod.</td>
+                                    <td>Qtd.Total</td>
+                                    <td>Vlr.Total</td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" style="text-align:right;" size="10" value="'. $qtdProdFalta . '" disabled=""/></td>
+                                    <td><input type="text" style="text-align:right;" size="15" value="'. $qtdTotalFalta . '" disabled=""/></td>
+                                    <td><input type="text" style="text-align:right;" size="15" value="'. $vlrTotalFalta . '" disabled=""/></td>
+                                </tr>
+                            </table>                
+                            </fieldset>                        
+                        </td>
+                        <td>
+                            <fieldset>
+                            <legend>Total das Divergencias</legend>
+                            <table width="100%">
+                                <tr>
+                                    <td>Qtd.Prod.</td>
+                                    <td>Qtd.Total</td>
+                                    <td>Vlr.Total</td>
+                                </tr>
+                                <tr>
+                                    <td><input type="text" style="text-align:right;" size="10" value="'. $qtdProdFalta . '" disabled=""/></td>
+                                    <td><input type="text" style="text-align:right;" size="15" value="'. $qtdTotalFalta . '" disabled=""/></td>
+                                    <td><input type="text" style="text-align:right;" size="15" value="'. $vlrTotalFalta . '" disabled=""/></td>
+                                </tr>
+                            </table>                
+                            </fieldset>                        
+                        </td>
+                    </tr>
+                </table>
+            </fieldset>';
+
         return $this;
     }
 
