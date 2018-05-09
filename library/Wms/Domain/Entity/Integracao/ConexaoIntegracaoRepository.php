cc016e5990cb480105806481d7fb8ac647917f64
Rodrigo Dantley <rodrigodantley@gmail.com>
2018-05-09 16:32:00 -0300

    conexao postegree

--- library/Wms/Domain/Entity/Integracao/ConexaoIntegracaoRepository.php
+++ library/Wms/Domain/Entity/Integracao/ConexaoIntegracaoRepository.php
@@ -172,7 +172,7 @@ class ConexaoIntegracaoRepository extends EntityRepository {
             $sid = $conexao->getDbName();
 
 
-            $dados_conexao = "host=192.168.0.220 dbname=sabium port=220 user=consulta.wms password=quebec@2018";
+            $dados_conexao = "host=192.168.0.220 dbname=sabium port=22 user=consulta.wms password=quebec@2018";
             if(!($conexao = pg_connect ($dados_conexao))) {
                 print "Não foi possível estabelecer uma conexão com o banco de dados.";
             } else {
