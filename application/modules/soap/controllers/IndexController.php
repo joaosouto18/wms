fd90a554512f1db56a0cd7861bd2f6f5dbe49dd7
Tarcisio <tarcisiocms@outlook.com>
2018-06-08 17:56:40 -0300

    Exception de conexão WSDL

--- application/modules/soap/controllers/IndexController.php
+++ application/modules/soap/controllers/IndexController.php
@@ -19,60 +19,77 @@ class Soap_IndexController extends Core\Controller\Action\WebService
     protected $serviceName;
 
 
+    protected $preException = null;
+
+
     public function init()
     {
-        parent::init();
+        try {
+            parent::init();
+            $service = $this->_getParam('service');
 
-        $parametroRepo = $this->em->getRepository('wms:Sistema\Parametro');
-        $parametro = $parametroRepo->findOneBy(array('constante' => 'ID_USER_ERP'));
+            if ($service == null) {
+                throw new \Exception('Serviço inválido');
+            }
+            $this->serviceName = $service;
+            $this->conf = new \Zend_Config_Ini(APPLICATION_PATH . '/configs/webservices.ini', APPLICATION_ENV);
 
-        if (!empty($parametro)){
-            $idUsuario = $parametro->getValor();
-        } else {
-            $idUsuario = APPLICATION_ENV == 'development' ? 142 : 1;
+            $this->getHelper('viewRenderer')->setNoRender(true);
+            $parametroRepo = $this->em->getRepository('wms:Sistema\Parametro');
+            $parametro = $parametroRepo->findOneBy(array('constante' => 'ID_USER_ERP'));
+
+            if (!empty($parametro)) {
+                $idUsuario = $parametro->getValor();
+            } else {
+                $idUsuario = APPLICATION_ENV == 'development' ? 142 : 1;
+            }
+
+            $usuario = $this->em->find('wms:Usuario', $idUsuario);
+
+            if (empty($usuario)) {
+                throw new \Exception("Usuário código: $idUsuario para webservice não cadastrado");
+            }
+
+            $perfis = array();
+            foreach ($usuario->getPerfis() as $perfil) {
+                $perfis[$perfil->getId()] = $perfil->getNome();
+            }
+            //ordeno crescentemente pelos codigos do perfil
+            ksort($perfis);
+            //gero o perfil
+            $perfil = implode('-', $perfis);
+            //seto o RoleId para o zf
+            $usuario->setRoleId($perfil);
+
+            $auth = \Zend_Auth::getInstance();
+            $storage = $auth->getStorage();
+            $storage->clear();
+            $storage->write($usuario);
+
+            $front = \Zend_Controller_Front::getInstance();
+            $front->setParam('noErrorHandler', true);
+            $front->setParam('noViewRenderer', true);
+
+            //desabilita layouts e views
+            if (null != \Zend_Layout::getMvcInstance()) {
+                \Zend_Layout::getMvcInstance()->disableLayout();
+            }
+
+        } catch (Exception $e) {
+            header('Content-Type: text/xml');
+            $msg = $e->getMessage();
+            $xmlResponse = "
+            <SOAP-ENV:Envelope xmlns:SOAP-ENV=\"http://schemas.xmlsoap.org/soap/envelope/\">
+               <SOAP-ENV:Body>
+                  <SOAP-ENV:Fault>
+                     <faultcode>Server Error</faultcode>
+                     <faultstring>$msg</faultstring>
+                  </SOAP-ENV:Fault>
+               </SOAP-ENV:Body>
+            </SOAP-ENV:Envelope>";
+
+            echo $xmlResponse;
         }
-
-        $usuario = $this->em->find('wms:Usuario', $idUsuario);
-
-        if (empty($usuario)) {
-            throw new \Exception("Usuário código: $idUsuario para webservice não cadastrado");
-        }
-
-        $perfis = array();
-        foreach ($usuario->getPerfis() as $perfil) {
-            $perfis[$perfil->getId()] = $perfil->getNome();
-        }
-        //ordeno crescentemente pelos codigos do perfil
-        ksort($perfis);
-        //gero o perfil
-        $perfil = implode('-', $perfis);
-        //seto o RoleId para o zf
-        $usuario->setRoleId($perfil);
-
-        $auth = \Zend_Auth::getInstance();
-        $storage = $auth->getStorage();
-        $storage->clear();
-        $storage->write($usuario);
-
-        $front = \Zend_Controller_Front::getInstance();
-        $front->setParam('noErrorHandler', true);
-        $front->setParam('noViewRenderer', true);
-
-        $this->conf = $conf = new \Zend_Config_Ini(APPLICATION_PATH . '/configs/webservices.ini', APPLICATION_ENV);
-
-        //desabilita layouts e views
-        if (null != \Zend_Layout::getMvcInstance()) {
-            \Zend_Layout::getMvcInstance()->disableLayout();
-        }
-
-        $this->getHelper('viewRenderer')->setNoRender(true);
-        $service = $this->_getParam('service');
-
-        if ($service == null) {
-            throw new \Exception('Serviço inválido');
-        }
-
-        $this->serviceName = $service;
     }
 
     /**
