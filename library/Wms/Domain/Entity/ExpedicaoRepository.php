4cc4a6d0034c0c3f63ced7d14a23cb80eff07388
Lucas Chinelate <lucaschinelate@hotmail.com>
2018-07-17 16:12:34 -0300

    Modificações

--- library/Wms/Domain/Entity/ExpedicaoRepository.php
+++ library/Wms/Domain/Entity/ExpedicaoRepository.php
@@ -26,6 +26,7 @@ class ExpedicaoRepository extends EntityRepository {
              LEFT JOIN MAPA_SEPARACAO MS ON MS.COD_EXPEDICAO = C.COD_EXPEDICAO
                  WHERE (ES.COD_STATUS = 522 OR P.IND_ETIQUETA_MAPA_GERADO = 'N' OR MS.COD_STATUS = 522)
                    AND P.DTH_CANCELAMENTO IS NULL
+                   AND P.COD_PEDIDO IN (SELECT COD_PEDIDO FROM PEDIDO_PRODUTO WHERE QUANTIDADE > NVL(QTD_CORTADA,0))
                    AND C.COD_EXPEDICAO = " . $idExpedicao;
         $result = $this->getEntityManager()->getConnection()->query($SQL)->fetchAll(\PDO::FETCH_ASSOC);
 
@@ -1801,6 +1802,7 @@ class ExpedicaoRepository extends EntityRepository {
                      AND E.COD_STATUS <> $statusCancelada
                      AND P.DTH_CANCELAMENTO IS NULL
                      AND P.CENTRAL_ENTREGA = $central
+                     AND P.COD_PEDIDO IN ( SELECT COD_PEDIDO FROM PEDIDO_PRODUTO WHERE QUANTIDADE > NVL(QTD_CORTADA,0))
                      AND E.IND_PROCESSANDO = 'N'
                    ";
 
