8bf8404b2073644263d334594a1a6132703a4df8
tarcisiocms <tarcisiocms@outlook.com>
2018-10-16 14:19:44 -0300

    Update nome de cliente

--- library/Wms/Domain/Entity/AtorRepository.php
+++ library/Wms/Domain/Entity/AtorRepository.php
@@ -257,4 +257,25 @@ class AtorRepository extends EntityRepository {
         return $this->getSystemParameterValue('PERMITE_CLIENTES_CNPJ_IGUAIS');
     }
 
+    /**
+     * @param $pessoaEn Fisica|Juridica
+     * @param $arrData
+     * @return Juridica|Fisica
+     */
+    public function tryUpdate( $pessoaEn, $arrData)
+    {
+        if (isset($arrData['nome']) && !empty($arrData['nome']) && $pessoaEn->getNome() != $arrData['nome']) {
+
+            $pessoaEn->setNome($arrData['nome']);
+
+            if (is_a($pessoaEn, "\Wms\Domain\Entity\Pessoa\Juridica")) {
+                $pessoaEn->setNomeFantasia($arrData['nome']);
+            }
+        }
+
+        $this->_em->persist($pessoaEn);
+
+        return $pessoaEn;
+    }
+
 }
