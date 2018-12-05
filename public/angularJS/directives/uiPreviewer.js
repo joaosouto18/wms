e138e8b8c354ab8ab5c2293bfd1533c4520778fb
Tarcisio <tarcisiocms@outlook.com>
2018-12-05 16:03:40 -0200

    inventarioNovo

---
+++ public/angularJS/directives/uiPreviewer.js
@@ -0,0 +1,8 @@
+angular.module('wms').directive("uiPreviewer", function () {
+    return {
+        templateUrl: 'uiPreviewer-inventario.html',
+        scope: {
+            title: "@"
+        }
+    }
+});
--- public/angularJS/diretivas/uiPreviewer.js
+++
@@ -1,5 +0,0 @@
-angular.module('wms').directive("uiPreviewer", function () {
-    return {
-        templateUrl: 'uiPreviewer-inventario.html'
-    }
-});
