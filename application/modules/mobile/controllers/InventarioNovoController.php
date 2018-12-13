b329f33614283b2684546bf28b04a11c8ed42b35
joaby <joaby.miranda@gmail.com>
2018-12-13 11:34:39 -0200

    correcao de listagem de inventarios no coletor

--- application/modules/mobile/controllers/InventarioNovoController.php
+++ application/modules/mobile/controllers/InventarioNovoController.php
@@ -15,27 +15,19 @@ class Mobile_InventarioNovoController extends Action
         /** @var \Wms\Domain\Entity\InventarioNovoRepository $inventarioRepo */
         $inventarioRepo = $this->em->getRepository('wms:InventarioNovo');
         $inventarios    = $inventarioRepo->findBy(['status' => \Wms\Domain\Entity\InventarioNovo::STATUS_LIBERADO]);
-        $this->view->inventarios = $inventarios;
+
+       $this->view->inventarios = $inventarios;
     }
 
     public function selecionaContagemAction()
     {
 
-        // pega o id do inventario pra recuperar a descricao
-
         // lógica para verificar o número da contagem
 
         // retornar as contagens pra view
 
-        /** @var \Wms\Service\Mobile\InventarioNovo $inventarioService */
-        $inventarioService               = $this->_service;
-        $idInventario                    = $this->_getParam('idInventario');
-        $descricaoInventario             = $this->_getParam('descricaoInventario');
-        $numContagensRegra               = $this->getSystemParameterValue('REGRA_CONTAGEM');
-        $numContagens                    = $inventarioService->getContagens(array('idInventario' => $idInventario, 'regraContagem' => $numContagensRegra));
-        $this->view->numContagens        = $numContagens;
-        $this->view->idInventario        = $idInventario;
-        $this->view->descricaoInventario = $descricaoInventario;
+        echo 'seleciona contagem';
+
 
     }
 
