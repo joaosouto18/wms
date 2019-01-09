6dc3d8ab725de66f101f7d0c26300e8c8261522d
Rodrigo Dantley <rodrigodantley@gmail.com>
2019-01-09 16:41:37 -0200

    cod_produto para integracao com winthor

--- library/Wms/Domain/Entity/Integracao/AcaoIntegracaoFiltroRepository.php
+++ library/Wms/Domain/Entity/Integracao/AcaoIntegracaoFiltroRepository.php
@@ -42,7 +42,6 @@ class AcaoIntegracaoFiltroRepository extends EntityRepository
                 $query = $acaoEn->getQuery();
             }
             $query = 'INSERT ALL '.implode(' ', $queryAll).' SELECT * FROM dual';
-            var_dump($query);exit;
         }elseif($insertAll === 'MSSQL'){
             $vetQuery = explode('VALUES', $query);
             $query = $vetQuery[1];
