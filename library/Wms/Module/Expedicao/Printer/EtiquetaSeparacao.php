9cbd2b3169ba209be660f00391480023405939cf
Rodrigo Dantley <rodrigodantley@gmail.com>
2019-01-14 10:05:32 -0200

    ajustes etiqueta separacao

--- library/Wms/Module/Expedicao/Printer/EtiquetaSeparacao.php
+++ library/Wms/Module/Expedicao/Printer/EtiquetaSeparacao.php
@@ -1093,6 +1093,7 @@ class EtiquetaSeparacao extends Pdf
         $this->MultiCell(38, 4.2, $impressao, 1, 'L');
         $this->SetX(30);
         $y2 = $this->getY();
+        $this->SetFont('Arial', 'B', 17);
         $impressao = "CARGA: $etiqueta[codCargaExterno] ";
         $this->MultiCell(40, 4.5, $impressao, 1, 'L');
         $this->SetY($y2);
