0600a031c339ee97d6fec30d078a497bb05ca55e
Rodrigo Dantley <rodrigodantley@gmail.com>
2019-02-18 17:43:36 -0300

    ajuste de integracao de produtos para exibir mensagem amigavel quando o codigo de barras ja existir

--- library/Wms/Domain/Entity/Integracao/AcaoIntegracaoRepository.php
+++ library/Wms/Domain/Entity/Integracao/AcaoIntegracaoRepository.php
@@ -374,8 +374,14 @@ class AcaoIntegracaoRepository extends EntityRepository
                         $ids = array();
                         foreach ($idTabelaTemp as $key => $value){
                             $ids[] = $value['ID'];
+                            if(count($ids) == $max){
+                                $ids = implode(',',$ids);
+                                $query = "UPDATE " . $acaoEn->getTabelaReferencia() . " SET IND_PROCESSADO = 'S', DTH_PROCESSAMENTO = SYSDATE WHERE ID IN ($ids) AND (IND_PROCESSADO IS NULL OR IND_PROCESSADO = 'N')";
+                                $this->_em->getConnection()->query($query)->execute();
+                                unset($ids);
+                            }
                         }
-                        if(count($ids) <= $max){
+                        if(count($ids) < $max){
                             $ids = implode(',',$ids);
                             $query = "UPDATE " . $acaoEn->getTabelaReferencia() . " SET IND_PROCESSADO = 'S', DTH_PROCESSAMENTO = SYSDATE WHERE ID IN ($ids) AND (IND_PROCESSADO IS NULL OR IND_PROCESSADO = 'N')";
                             $this->_em->getConnection()->query($query)->execute();
