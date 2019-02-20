f0a203aac8f7e8ea8b98986a62fa7b51e446469f
Rodrigo Dantley <rodrigodantley@gmail.com>
2019-02-20 12:24:00 -0300

    atualizar tabelas temporarias de triggers

--- library/Wms/Module/Armazenagem/Printer/EtiquetaEndereco.php
+++ library/Wms/Module/Armazenagem/Printer/EtiquetaEndereco.php
@@ -414,7 +414,6 @@ class EtiquetaEndereco extends Pdf
         $this->SetFont('Arial', 'B', 13.5);
 //        $this->Cell(30,13,"",0,0, 'C');
         $this->Cell(0,13,substr(reset($produto)['codProduto'].' - '.reset($produto)['descricao'],0,35),0,1,'C');
-        $this->Cell(17,13,"",0,0);
         $this->SetFont('Arial', 'B', 12);
         $this->SetX(20);
         $this->Cell($wRua,20, utf8_decode("RUA"),0,0);
