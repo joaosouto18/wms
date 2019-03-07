549517dd7081ff1503e61006a0bc951372f35e13
Rodrigo Dantley <rodrigodantley@gmail.com>
2019-03-07 14:17:07 -0300

    envio de produtos por integracao

--- application/modules/web/controllers/ProdutoController.php
+++ application/modules/web/controllers/ProdutoController.php
@@ -44,7 +44,7 @@ class Web_ProdutoController extends Crud {
             $acaoIntegracaoRepository = $this->getEntityManager()->getRepository('wms:Integracao\AcaoIntegracao');
             foreach ($explodeIntegracoes as $codIntegracao) {
                 $acaoIntegracaoEntity = $acaoIntegracaoRepository->find($codIntegracao);
-                $acaoIntegracaoRepository->processaAcaoProduto($acaoIntegracaoEntity);
+                $acaoIntegracaoRepository->processaAcao($acaoIntegracaoEntity);
             }
         }
 
