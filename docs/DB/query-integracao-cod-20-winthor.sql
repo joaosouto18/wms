6690613504d9615ca2bd8125712bab809c6a814b
Lucas Chinelate <lucaschinelate@hotmail.com>
2019-03-22 15:55:01 -0300

    modificacoes

--- docs/DB/query-integracao-cod-20-winthor.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, 'x.x.x', 'query-integracao-cod-20-winthor.sql');
-
-
-update acao_integracao_filtro set dsc_filtro = ' AND (nf.numnota IN (:?1) OR c.numcar IN (:?2)) ' where cod_acao_integracao = 20 and cod_tipo_registro = 612;
