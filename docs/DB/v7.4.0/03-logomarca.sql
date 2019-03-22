da33c576172ea42c4e7dd8ad96ccf9f6305e61ee
Lucas Chinelate <lucaschinelate@hotmail.com>
2019-03-22 15:56:07 -0300

    scripts

--- docs/DB/v7.4.0/03-logomarca.sql
+++ docs/DB/v7.4.0/03-logomarca.sql
@@ -1,4 +1,6 @@
 /* INSERÇÃO DE LOGOMARCA DO CLIENTE */
+INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, '7.4.0','03-logomarca');
+
 INSERT INTO acao (cod_acao, dsc_acao, nom_acao) VALUES (SQ_ACAO_01.NEXTVAL, 'Inserir logomarca', 'logomarca');
 
 INSERT INTO recurso_acao ( cod_recurso_acao, cod_recurso, cod_acao, dsc_recurso_acao )
