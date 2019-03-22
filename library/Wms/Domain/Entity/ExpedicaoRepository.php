2276be84f79fc51092c7e71311a0925f1539da14
Rodrigo Dantley <rodrigodantley@gmail.com>
2019-03-22 18:01:21 -0300

    correcao de geracao de mapa de separacao

--- library/Wms/Domain/Entity/ExpedicaoRepository.php
+++ library/Wms/Domain/Entity/ExpedicaoRepository.php
@@ -1550,7 +1550,7 @@ class ExpedicaoRepository extends EntityRepository {
         }
 
         
-        $query = "SELECT pp
+/*        $query = "SELECT pp
                         FROM wms:Expedicao\PedidoProduto pp
                         INNER JOIN pp.produto p
                          LEFT JOIN p.linhaSeparacao ls
@@ -1566,20 +1566,20 @@ class ExpedicaoRepository extends EntityRepository {
                           AND ped.centralEntrega = '$central'
                           AND ped.dataCancelamento is null
                         ";
-          
+          */
 
-//        $query = "SELECT pp
-//                        FROM wms:Expedicao\PedidoProduto pp
-//                        INNER JOIN pp.produto p
-//                         LEFT JOIN p.linhaSeparacao ls
-//                        INNER JOIN pp.pedido ped
-//                        INNER JOIN wms:Expedicao\VProdutoEndereco e WITH p.id = e.codProduto AND p.grade = e.grade
-//                        INNER JOIN ped.carga c
-//                        WHERE ped.indEtiquetaMapaGerado != 'S'
-//                          $whereCargas
-//                          AND ped.centralEntrega = '$central'
-//                          AND ped.dataCancelamento is null
-//                        ";
+        $query = "SELECT pp
+                        FROM wms:Expedicao\PedidoProduto pp
+                        INNER JOIN pp.produto p
+                         LEFT JOIN p.linhaSeparacao ls
+                        INNER JOIN pp.pedido ped
+                        INNER JOIN wms:Expedicao\VProdutoEndereco e WITH p.id = e.codProduto AND p.grade = e.grade
+                        INNER JOIN ped.carga c
+                        WHERE ped.indEtiquetaMapaGerado != 'S'
+                          $whereCargas
+                          AND ped.centralEntrega = '$central'
+                          AND ped.dataCancelamento is null
+                        ";
 
         switch ($sequencia) {
             case 3:
