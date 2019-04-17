6714b03dc6b73c0d5b0545bbc3ed1a87a745b70b
Lucas Chinelate <lucaschinelate@hotmail.com>
2019-04-17 09:35:30 -0400

    Correção na integração de peso

--- application/modules/web/controllers/ProdutoController.php
+++ application/modules/web/controllers/ProdutoController.php
@@ -271,11 +271,11 @@ class Web_ProdutoController extends Crud {
                             $paramsSave['embalagens'][$key]['capacidadePicking'] = $fator * $paramsSave['embalagem']['capacidadePicking'];
                             $paramsSave['embalagens'][$key]['pontoReposicao'] = $fator * $paramsSave['embalagem']['pontoReposicao'];
                             $paramsSave['embalagens'][$key]['endereco'] = $paramsSave['embalagem']['endereco'];
-                            $paramsSave['embalagens'][$key]['altura'] = number_format($altura, 3, ',', '');
-                            $paramsSave['embalagens'][$key]['profundidade'] = $paramsSave['embalagem']['profundidade'];
-                            $paramsSave['embalagens'][$key]['cubagem'] = number_format($cubagem, 4, ',', '');
-                            $paramsSave['embalagens'][$key]['peso'] = number_format($peso, 3, ',', '');
-                            $paramsSave['embalagens'][$key]['largura'] = $paramsSave['embalagem']['largura'];
+                            $paramsSave['embalagens'][$key]['altura'] = $altura; //number_format($altura, 3, ',', '');
+                            $paramsSave['embalagens'][$key]['profundidade'] = $profundidade; // $paramsSave['embalagem']['profundidade'];
+                            $paramsSave['embalagens'][$key]['cubagem'] = $cubagem; //number_format($cubagem, 4, ',', '');
+                            $paramsSave['embalagens'][$key]['peso'] = $peso ; //number_format($peso, 3, ',', '');
+                            $paramsSave['embalagens'][$key]['largura'] = $largura; //e$paramsSave['embalagem']['largura'];
                             if (empty($paramsSave['embalagens'][$key]['acao'])) {
                                 $paramsSave['embalagens'][$key]['acao'] = 'alterar';
                             }
