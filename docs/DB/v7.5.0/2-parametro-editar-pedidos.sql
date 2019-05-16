6370c1f1f514335f98201421014bdfb7d3aaba57
Lucas Chinelate <lucaschinelate@hotmail.com>
2019-05-16 11:53:33 -0300

    docs

--- docs/DB/v7.5.0/2-parametro-editar-pedidos.sql
+++ docs/DB/v7.5.0/2-parametro-editar-pedidos.sql
@@ -7,17 +7,17 @@
 DECLARE
   CHECK_RESULT VARCHAR2(100);
 BEGIN
-  SELECT FUNC_CHECK_SCRIPT('parametro-caracter-especial-emb-fracionavel.sql', '') INTO CHECK_RESULT FROM DUAL;
+  SELECT FUNC_CHECK_SCRIPT('2-parametro-editar-pedidos.sql', '7.5.0') INTO CHECK_RESULT FROM DUAL;
   IF (CHECK_RESULT <> 'TRUE') THEN
     DBMS_OUTPUT.PUT_LINE(CHECK_RESULT);
   ELSE
-    INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, '7.5', 'parametro-caracter-especial-emb-fracionavel.sql');
+    INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, '7.5.0', '2-parametro-editar-pedidos.sql');
     /************************************************************************
 **        COLOQUE O SCRIPT À SER EXECUTADO ENTRE ESTA DEMARCAÇÃO       **
 ************************************************************************/
 
     insert into parametro (cod_parametro,cod_contexto_parametro,dsc_parametro,dsc_titulo_parametro,ind_parametro_sistema,cod_tipo_atributo,dsc_valor_parametro)
-    values (sq_parametro_01.nextval, (select cod_contexto_parametro from contexto_parametro where dsc_contexto_parametro = 'PARÂMETROS DE INTEGRAÇÃO'), 'CARACTERE_ESPECIAL_COD_BARRAS','Aplicar # no código de barras das embalagens fracionáveis','S','A','N');
+    values (sq_parametro_01.nextval, (select cod_contexto_parametro from contexto_parametro where dsc_contexto_parametro = 'PARÂMETROS DE INTEGRAÇÃO'), 'PERMITIR_ALTERAR_PEDIDOS','Permite alterar Pedidos separados/conferidos','S','A','N');
 
     /************************************************************************
 **                 NÃO ALTERAR ABAIXO DESTA REGIÃO                     **
