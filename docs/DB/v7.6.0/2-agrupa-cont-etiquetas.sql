380991498a6d435bdf5c2332d50a27a08a80ec23
Lucas Chinelate <lucaschinelate@hotmail.com>
2019-07-12 17:36:53 -0300

    MERGE

--- docs/DB/v7.6.0/2-agrupa-cont-etiquetas.sql
+++ docs/DB/v7.6.0/2-agrupa-cont-etiquetas.sql
@@ -7,11 +7,11 @@
 DECLARE
     CHECK_RESULT VARCHAR2(100);
 BEGIN
-    SELECT FUNC_CHECK_SCRIPT('x-agrupa-cont-etiquetas.sql', '') INTO CHECK_RESULT FROM DUAL;
+    SELECT FUNC_CHECK_SCRIPT('2-agrupa-cont-etiquetas.sql', '') INTO CHECK_RESULT FROM DUAL;
     IF (CHECK_RESULT <> 'TRUE') THEN
         DBMS_OUTPUT.PUT_LINE(CHECK_RESULT);
     ELSE
-        INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, '7.6', 'x-agrupa-cont-etiquetas.sql');
+        INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, '7.6', '2-agrupa-cont-etiquetas.sql');
         /************************************************************************
 **        COLOQUE O SCRIPT À SER EXECUTADO ENTRE ESTA DEMARCAÇÃO       **
 ************************************************************************/
