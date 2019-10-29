284b6b1f2a3566d4f7cbacfdb4c11d31b09d9549
Lucas Chinelate <lucaschinelate@hotmail.com>
2019-10-29 00:37:10 -0300

    fechando versao

--- docs/DB/v7.8.0/07-integracao-lote-banco-dados.sql
+++ docs/DB/v7.8.0/07-integracao-lote-banco-dados.sql
@@ -7,11 +7,11 @@
 DECLARE
     CHECK_RESULT VARCHAR2(100);
 BEGIN
-    SELECT FUNC_CHECK_SCRIPT('xx-integracao-lote-banco-dados.sql') INTO CHECK_RESULT FROM DUAL;
+    SELECT FUNC_CHECK_SCRIPT('-7-integracao-lote-banco-dados.sql') INTO CHECK_RESULT FROM DUAL;
     IF (CHECK_RESULT <> 'TRUE') THEN
         DBMS_OUTPUT.PUT_LINE(CHECK_RESULT);
     ELSE
-        INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, '7.8', 'xx-integracao-lote-banco-dados.sql');
+        INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, '7.8', '07-integracao-lote-banco-dados.sql');
         /************************************************************************
 **        COLOQUE O SCRIPT À SER EXECUTADO ENTRE ESTA DEMARCAÇÃO       **
 ************************************************************************/
