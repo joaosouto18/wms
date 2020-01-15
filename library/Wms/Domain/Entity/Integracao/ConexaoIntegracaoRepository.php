b81bcdeea33a200f355cea576985fedbbcb54907
Rodrigo Dantley <rodrigodantley@gmail.com>
2020-01-15 10:03:12 -0300

    correções postgre

--- library/Wms/Domain/Entity/Integracao/ConexaoIntegracaoRepository.php
+++ library/Wms/Domain/Entity/Integracao/ConexaoIntegracaoRepository.php
@@ -234,7 +234,7 @@ class ConexaoIntegracaoRepository extends EntityRepository {
             $arr = pg_fetch_all($result);
 
             if (!$arr)
-                $arr = null;
+                $arr = array();
 
             pg_close($conexao);
             return $arr;
