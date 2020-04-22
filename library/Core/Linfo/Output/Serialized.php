35078078b0f50d7ec4265d4e886a09080c189324
Lucas Chinelate <lucaschinelate@hotmail.com>
2020-04-22 15:11:03 -0300

    Linfo

---
+++ library/Core/Linfo/Output/Serialized.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Core\Linfo\Output;
+
+use Core\Linfo\Linfo;
+
+class Serialized implements Output
+{
+    protected $linfo;
+
+    public function __construct(Linfo $linfo)
+    {
+        $this->linfo = $linfo;
+    }
+
+    public function output()
+    {
+        $settings = $this->linfo->getSettings();
+
+        // Output buffering, along with compression (if supported)
+        if (!isset($settings['compress_content']) || $settings['compress_content']) {
+            ob_start(function_exists('ob_gzhandler') ? 'ob_gzhandler' : null);
+        }
+
+        echo serialize($this->linfo->getInfo());
+    }
+}
