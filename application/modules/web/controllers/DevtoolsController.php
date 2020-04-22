0a1cb230435f4295c9ced96ea700051c8eb7af72
Lucas Chinelate <lucaschinelate@hotmail.com>
2020-04-21 21:17:59 -0300

    devtools

---
+++ application/modules/web/controllers/DevtoolsController.php
@@ -0,0 +1,16 @@
+<?php
+
+use \Wms\Module\Web\Controller\Action\Crud;
+
+/**
+ * Description of SystemParamsController
+ *
+ * @author Renato Medina <medinadato@gmail.com>
+ */
+class Web_DevtoolsController extends \Wms\Controller\Action
+{
+
+    public function indexAction()
+    {
+    }
+}
