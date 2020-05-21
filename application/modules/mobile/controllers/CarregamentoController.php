7031acbb181b09d3cd8847b046e32333c30251a1
Tarcisio <tarcisiocms@outlook.com>
2020-05-21 00:59:18 -0300

    Single page conferencia

--- application/modules/mobile/controllers/CarregamentoController.php
+++ application/modules/mobile/controllers/CarregamentoController.php
@@ -5,6 +5,25 @@ class Mobile_CarregamentoController extends \Wms\Controller\Action
 {
     public function indexAction()
     {
-        echo "Chegou";
+        $this->view->isOldBrowserVersion = $this->getOldBrowserVersion();
+    }
+
+    public function getInfoDanfeAction()
+    {
+
+        $response = [
+            'codExpedicao' => 508,
+            'clientes' => [
+                321 => [
+                    'nome' => 'Cliente Teste',
+                    'danfes' => [
+                        '32158456214511251454188546632548573529185248' => false,
+                        '51454188546632548321584562145112573529185248' => false
+                    ]
+                ]
+            ]
+        ];
+
+        $this->_helper->json(['status' => 'ok', 'response' => $response]);
     }
 }
