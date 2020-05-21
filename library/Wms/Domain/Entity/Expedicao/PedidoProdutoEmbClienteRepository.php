062ff410da705f5220b9ca828df4f5616a02641b
Lucas Chinelate <lucaschinelate@hotmail.com>
2020-05-21 15:13:52 -0300

    Modificacao CCB

---
+++ library/Wms/Domain/Entity/Expedicao/PedidoProdutoEmbClienteRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Wms\Domain\Entity\Expedicao;
+
+use Doctrine\ORM\EntityRepository;
+use Wms\Domain\Entity\Expedicao;
+use Wms\Domain\Configurator;
+use Wms\Domain\Entity\Produto\Lote;
+use Wms\Domain\Entity\Ressuprimento\ReservaEstoqueExpedicaoRepository;
+
+class PedidoProdutoEmbClienteRepository extends EntityRepository
+{
+
+}
