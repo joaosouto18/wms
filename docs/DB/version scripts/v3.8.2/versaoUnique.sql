510ce780ce9c4150d4ab85b08bcb777cd04154de
Tarcisio <tarcisiocms@outlook.com>
2020-07-08 15:47:13 -0300

    Ajuste

--- docs/DB/version scripts/v3.8.2/versaoUnique.sql
+++
@@ -1,5 +0,0 @@
-/* Rode primeiro essa linha, caso de problema de unique key, significa que esse arquivo .sql já foi executado anteriormente */
-INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, '3.8.2', 'versaoUnique.sql');
-
-/* Conteúdo do arquivo */
-CREATE UNIQUE INDEX VERSAO_SCRIPT ON VERSAO (SCRIPT);
--- docs/DB/version scripts/v7.11/02-selecionar-nota-divergencia.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO VERSAO (DTH, NUMERO_VERSAO, SCRIPT) VALUES (SYSDATE, '7.11.0', '02-selecionar-nota-divergencia.sql');
-
-alter table nota_fiscal
-    add ind_divergencia CHAR(1) default 'N' null;
-
-update nota_fiscal set ind_divergencia = 'S' where cod_nota_fiscal in (
-    select distinct cod_nota_fiscal from recebimento_conferencia where cod_nota_fiscal is not null);
