a464c29d08af34ed9974b5b344d9fdccc27a1804
Lucas Chinelate <lucaschinelate@hotmail.com>
2020-07-11 09:49:12 -0300

    Executando consultas, ordenacao e export

--- application/modules/web/controllers/RelatorioCustomizadoController.php
+++ application/modules/web/controllers/RelatorioCustomizadoController.php
@@ -8,26 +8,90 @@ class Web_RelatorioCustomizadoController extends Action
     }
 
     public function relatorioAction (){
-        $Query = "SELECT COD_PRODUTO as Codigo, DSC_PRODUTO 
-                    FROM PRODUTO P 
-                   WHERE 1 = 1 @CodProduto";
+        $params = $this->getRequest()->getParams();
 
         $paramsQuery = array();
+        $sort = arraY();
+        $titulo = "";
+        $query = "";
+
+        /*
+         * Esta parte é resultado de consulta em banco
+         * montada na mão apenas para desenvolvimento da regra de negocio
+         */
+
+        //Dados do relatório
+        $query = "SELECT COD_PRODUTO as Codigo, DSC_PRODUTO 
+                    FROM PRODUTO P 
+                   WHERE 1 = 1 :CodProduto :DscProduto";
+        $titulo = "Relatório de Produtos";
+
+        //Critérios de Filtro
         $paramsQuery[] = array(
             'name' => 'CodProduto',
             'label' => 'Código',
-            'query' => " AND P.COD_PRODUTO = '@Value' ",
+            'query' => " AND P.COD_PRODUTO = ':value' ",
+            'required' => "N",
             'type' => 'text'
         );
         $paramsQuery[] = array(
             'name' => 'DscProduto',
             'label' => 'Descrição',
-            'query' => " AND P.DSC_PRODUTO LIKE '%@Value%' ",
+            'required' => "N",
+            'query' => " AND P.DSC_PRODUTO LIKE '%:value%' ",
             'type' => 'text'
         );
 
+        //Critérios de Ordenação
+        $sort[] = array(
+            'label' => 'Código ASC',
+            'value' => 'P.COD_PRODUTO ASC'
+        );
+        $sort[] = array(
+            'label' => 'Descrição ASC',
+            'value' => 'P.DSC_PRODUTO ASC'
+        );
+
+
+
         $form = new \Wms\Module\Web\Form\RelatorioCustomizado($paramsQuery);
-        $form->init($paramsQuery);
+        $form->init($paramsQuery, $sort);
+        if (isset($params['btnBuscar']) || isset($params['btnPDF']) || isset($params['btnXLS'])) {
+
+            foreach ($paramsQuery as $value) {
+                $filterValue = "";
+                if (isset($params[$value['name']]) && $params[$value['name']] != null)
+                    $filterValue = $params[$value['name']];
+
+                if ($filterValue != '') {
+                    $filterValue = str_replace(':value' , $filterValue, $value['query']);
+                }
+
+                $query = str_replace(":" . $value['name'], $filterValue, $query );
+            }
+
+            if (isset($params['sort']) && $params['sort'] != null) {
+                $query .= " ORDER BY " . $params['sort'];
+            }
+
+            $result = $this->getEntityManager()->getConnection()->query($query)->fetchAll(\PDO::FETCH_ASSOC);
+            $form->setDefaults($params);
+
+            if (count($result) == 0) {
+                $this->addFlashMessage('info', 'Nenhum Resultado Encontrado');
+            } else {
+                if (isset($params['btnBuscar'])) {
+                    $this->view->result = $result;
+                }
+                if (isset($params['btnPDF'])) {
+                    $this->exportPDF($result,  $titulo, $titulo,'L');
+                }
+                if (isset($params['btnXLS'])) {
+                    $this->exportCSV($result, $titulo,true );
+                }
+            }
+        }
+        $this->view->title = $titulo;
         $this->view->form = $form;
 
     }
--- library/Wms/Module/Web/Form/RelatorioCustomizado.php
+++ library/Wms/Module/Web/Form/RelatorioCustomizado.php
@@ -6,24 +6,51 @@ use Wms\Module\Web\Form;
 
 class RelatorioCustomizado extends Form
 {
-    public function init($params = null)
+    public function init($params = null, $sort = null)
     {
-        $elements = array();
         if (isset($params) && ($params!= null)) {
             foreach ($params as $param) {
+                $required = false;
+                if ($param['required'] == "S") $required = true;
                 $this->addElement($param['type'], $param['name'], array(
-                    'label' => $param['label']
+                    'label' => $param['label'],
+                    'required' => $required
                 ));
-                $elements[] = $param['name'];
             }
-
         }
 
-        $this->addElement('button', 'btnBuscar', array(
+        if (isset($sort) && ($sort!= null)) {
+            $srt = array();
+            $firstOpt = "";
+            foreach ($sort as $srtOption) {
+                if ($firstOpt == "") $firstOpt = $srtOption['value'];
+                $srt[$srtOption['value']] = $srtOption['label'];
+            }
+
+            $this->addElement('select', 'sort', array(
+                'label' => 'Ordenação',
+                'multiOptions' => $srt,
+                'required' => true,
+                'value' => $firstOpt
+            ));
+        }
+
+        $this->addElement('submit', 'btnBuscar', array(
             'class' => 'btn',
-            'label' => 'Buscar'
+            'label' => 'Buscar',
+            'decorators' => array('ViewHelper'),
         ));
-        $elements[] = 'btnBuscar';
-        $this->addDisplayGroup($elements, 'filtro', array('legend' => 'Filtro'));
+        $this->addElement('submit', 'btnPDF', array(
+            'class' => 'btn',
+            'label' => 'PDF',
+            'decorators' => array('ViewHelper'),
+        ));
+        $this->addElement('submit', 'btnXLS', array(
+            'class' => 'btn',
+            'label' => 'EXCEL',
+            'decorators' => array('ViewHelper'),
+        ));
+
+        $this->addDisplayGroup($this->getElements(), 'filtro', array('legend' => 'Filtro'));
     }
 }
