b67f3c2ac2b9c397f2629165f2658d4ba623418c
Rodrigo Dantley <rodrigodantley@gmail.com>
2020-08-06 10:16:10 -0300

    correção informar q nota saida foi integrada

--- library/Wms/Domain/Entity/Expedicao/ConferenciaCarregamentoRepository.php
+++ library/Wms/Domain/Entity/Expedicao/ConferenciaCarregamentoRepository.php
@@ -177,8 +177,8 @@ class ConferenciaCarregamentoRepository extends EntityRepository
                     ]
                 ];
             } else {
-                $expInfo['clientes'][$item['COD_PESSOA']]['totalDanfes'] = count($expInfo['clientes'][$item['COD_PESSOA']]['danfes']);
                 $expInfo['clientes'][$item['COD_PESSOA']]['danfes'][$item['COD_CHAVE_ACESSO']] = ['status' => false, 'nota' => $item['NOTA']];
+                $expInfo['clientes'][$item['COD_PESSOA']]['totalDanfes'] = count($expInfo['clientes'][$item['COD_PESSOA']]['danfes']);
             }
         }
 
