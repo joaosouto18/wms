75e0db91fa2de4b314b272a17f47b122ec432314
Lucas Chinelate <lucaschinelate@hotmail.com>
2020-08-06 13:29:33 -0300

    Comparativo de Inventario com ERP

--- application/modules/inventario_novo/controllers/ComparativoInventarioController.php
+++ application/modules/inventario_novo/controllers/ComparativoInventarioController.php
@@ -11,13 +11,34 @@ class Inventario_Novo_ComparativoInventarioController  extends Action
     public function indexAction()
     {
 
-        $params = $this->getRequest()->getParams();
-        $modeloExportacao = $this->getSystemParameterValue("MODELO_EXPORTACAO_INVENTARIO");
-        $invRepo = $this->getEntityManager()->getRepository("wms:InventarioNovo");
-
-        $filtroForm = new \Wms\Module\InventarioNovo\Form\ComparativoInventarioForm();
         try {
 
+            $params = $this->getRequest()->getParams();
+
+            /** @var \Wms\Domain\Entity\InventarioNovoRepository $invRepo */
+            $invRepo = $this->getEntityManager()->getRepository("wms:InventarioNovo");
+            /** @var \Wms\Domain\Entity\Integracao\AcaoIntegracaoRepository $acaoIntRepo */
+            $acaoIntRepo = $this->getEntityManager()->getRepository('wms:Integracao\AcaoIntegracao');
+
+            $modeloExportacao = $this->getSystemParameterValue("MODELO_EXPORTACAO_INVENTARIO");
+            $idAcao = $this->getSystemParameterValue("ID_INTEGRACAO_COMPARATIVO_INVENTARIO_ERP");
+
+            if (($modeloExportacao != 1) && ($modeloExportacao !=3) && ($modeloExportacao != 4)) {
+                throw new \Exception("Modelo de exportação de inventário não compatível com esta funcionalidade");
+            }
+            if ($idAcao == "") {
+                throw new \Exception("Para acessar este recurso é preciso que uma integração de comparação de inventário com ERP esteja configurada");
+            }
+
+            $acaoEn = $acaoIntRepo->find($idAcao);
+            if ($acaoEn == null) {
+                throw new \Exception("Integração de comparação de inventário com ERP não encontrada para o ID especificado no parametro");
+            }
+            if ($acaoEn->getTipoAcao()->getId() != \Wms\Domain\Entity\Integracao\AcaoIntegracao::INTEGRACAO_COMPARATIVO_INVENTARIO_ERP) {
+                throw new \Exception("Integração de Comparativo de Inventário com ERP Mal configurada");
+            }
+
+            $filtroForm = new \Wms\Module\InventarioNovo\Form\ComparativoInventarioForm();
             if (isset($params['btnSubmit']) || isset($params['btnExport'])) {
                 $idInventarioERP = $params['codInventario'];
                 $inv = $invRepo->findOneBy(array('codErp' => $idInventarioERP));
@@ -30,15 +51,21 @@ class Inventario_Novo_ComparativoInventarioController  extends Action
                 $idInventario = $params['codInventario'];
                 $invWMS = $this->getServiceLocator()->getService("Inventario")->getResultadoInventarioComparativo($modeloExportacao,$idInventario);
 
-                $invERP = array();
-                $invERP[] = array('COD_PRODUTO' => '8', 'DSC_GRADE' => 'UNICA', 'DSC_PRODUTO' => 'OVOS BRANCOS GRANDES "A" GRANEL CX 30 DZ');
-                $invERP[] = array('COD_PRODUTO' => '5917', 'DSC_GRADE' => 'UNICA', 'DSC_PRODUTO' => 'FRANGO CONGELADO MAROMBI CX17KG	');
-                $invERP[] = array('COD_PRODUTO' => '570', 'DSC_GRADE' => 'UNICA', 'DSC_PRODUTO' => 'ESPET GRAN BOVINO AURORA 40X100G CX 4KG	');
-                $invERP[] = array('COD_PRODUTO' => '1013', 'DSC_GRADE' => 'UNICA', 'DSC_PRODUTO' => 'Produto de Teste 1013');
+                /*
+                 * MOCK PARA TESTES E DEMONSTRAÇÃO
+                 $invERP = array();
+                 $invERP[] = array('COD_PRODUTO' => '8', 'DSC_GRADE' => 'UNICA', 'DSC_PRODUTO' => 'OVOS BRANCOS GRANDES "A" GRANEL CX 30 DZ');
+                 $invERP[] = array('COD_PRODUTO' => '5917', 'DSC_GRADE' => 'UNICA', 'DSC_PRODUTO' => 'FRANGO CONGELADO MAROMBI CX17KG	');
+                 $invERP[] = array('COD_PRODUTO' => '570', 'DSC_GRADE' => 'UNICA', 'DSC_PRODUTO' => 'ESPET GRAN BOVINO AURORA 40X100G CX 4KG	');
+                 $invERP[] = array('COD_PRODUTO' => '1013', 'DSC_GRADE' => 'UNICA', 'DSC_PRODUTO' => 'Produto de Teste 1013');
+                 */
 
+                $options = array(0 => $idInventario);
+                $invERP = $acaoIntRepo->processaAcao($acaoEn,$options,'E','P',null, \Wms\Domain\Entity\Integracao\AcaoIntegracaoFiltro::CODIGO_ESPECIFICO);
                 if (count($invERP) == 0) {
                     throw new \Exception("Inventário no ERP código " . $idInventario . " não encontrado ou sem nenhum produto definido");
                 }
+
                 $filtroForm->init(true);
 
                 $result = $this->getServiceLocator()->getService("Inventario")->comparataInventarioWMSxERP($invWMS, $invERP);
@@ -74,8 +101,10 @@ class Inventario_Novo_ComparativoInventarioController  extends Action
             $this->addFlashMessage('error', $e->getMessage());
         }
 
-        $filtroForm->setDefaults($params);
-        $this->view->form = $filtroForm;
+        if (isset($filtroForm)) {
+            $filtroForm->setDefaults($params);
+            $this->view->form = $filtroForm;
+        }
 
     }
 }
